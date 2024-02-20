fcffaf966bcabf719ee6b96b0b22632fd9f23219
koyopro <koyopro2.0@gmail.com>
2024-02-21 02:53:07 +0900

    refactor types

--- packages/accel-record-core/src/persistence.ts
+++ packages/accel-record-core/src/persistence.ts
@@ -7,12 +7,15 @@ export class Persistence {
   isNewRecord: boolean = true;
 
   save<T extends Fields & Connection & Persistence>(this: T): boolean {
-    const ret = this.createOrUpdate();
+    const ret = createOrUpdate(this);
     this.isNewRecord = false;
     return ret;
   }
 
-  update<T extends Fields & Connection & Persistence & AttributeAssignment>(this: T, data: Partial<T>): boolean {
+  update<T extends Fields & Connection & Persistence & AttributeAssignment>(
+    this: T,
+    data: Partial<T>
+  ): boolean {
     this.assignAttributes(data);
     return this.save();
   }
@@ -26,62 +29,66 @@ export class Persistence {
     rpcClient(query);
     return true;
   }
-
-  protected createOrUpdate<T extends Fields & Connection & Persistence>(this: T): boolean {
-    // if (this.readonly) {
-    //   throw new Error("Readonly record");
-    // }
-    // if (this.destroyed) {
-    //   return false;
-    // }
-    return this.isNewRecord ? this.createRecord() : this.updateRecord();
-  }
-
-  protected updateRecord<T extends Fields & Connection & Persistence>(this: T): boolean {
-    const where = {} as Record<keyof T, any>;
-    for (const key of this.primaryKeys as (keyof T)[]) {
-      where[key] = this[key];
-    }
-    const data: any = {};
-    for (const column of this.columns as (keyof T)[]) {
-      if (this[column] !== undefined) {
-        data[column as string] = this[column];
-      }
-    }
-    const query = this.client.where(where).update(data).toSQL();
-    const id = rpcClient(query);
-    (this as any).id = id;
-    for (const [key, { foreignKey }] of Object.entries(this.assosiations)) {
-      const value = this[key as keyof T];
-      if (Array.isArray(value)) {
-        for (const instance of value) {
-          instance[foreignKey] = id;
-          instance.save();
-        }
-      }
-    }
-    return true;
-  }
-
-  protected createRecord<T extends Fields & Connection>(this: T): boolean {
-    const data: any = {};
-    for (const column of this.columns as (keyof T)[]) {
-      if (this[column] !== undefined) {
-        data[column as string] = this[column];
-      }
-    }
-    const query = this.client.insert(data).toSQL();
-    const id = rpcClient(query);
-    (this as any).id = id;
-    for (const [key, { foreignKey }] of Object.entries(this.assosiations)) {
-      const value = this[key as keyof T];
-      if (Array.isArray(value)) {
-        for (const instance of value) {
-          instance[foreignKey] = id;
-          instance.save();
-        }
-      }
-    }
-    return true;
-  }
 }
+
+const createOrUpdate = <T extends Fields & Connection & Persistence>(
+  obj: T
+): boolean => {
+  // if (this.readonly) {
+  //   throw new Error("Readonly record");
+  // }
+  // if (this.destroyed) {
+  //   return false;
+  // }
+  return obj.isNewRecord ? createRecord(obj) : updateRecord(obj);
+};
+
+const updateRecord = <T extends Fields & Connection & Persistence>(
+  obj: T
+): boolean => {
+  const where = {} as Record<keyof T, any>;
+  for (const key of obj.primaryKeys as (keyof T)[]) {
+    where[key] = obj[key];
+  }
+  const data: any = {};
+  for (const column of obj.columns as (keyof T)[]) {
+    if (obj[column] !== undefined) {
+      data[column as string] = obj[column];
+    }
+  }
+  const query = obj.client.where(where).update(data).toSQL();
+  const id = rpcClient(query);
+  (obj as any).id = id;
+  for (const [key, { foreignKey }] of Object.entries(obj.assosiations)) {
+    const value = obj[key as keyof T];
+    if (Array.isArray(value)) {
+      for (const instance of value) {
+        instance[foreignKey] = id;
+        instance.save();
+      }
+    }
+  }
+  return true;
+};
+
+const createRecord = <T extends Fields & Connection>(obj: T): boolean => {
+  const data: any = {};
+  for (const column of obj.columns as (keyof T)[]) {
+    if (obj[column] !== undefined) {
+      data[column as string] = obj[column];
+    }
+  }
+  const query = obj.client.insert(data).toSQL();
+  const id = rpcClient(query);
+  (obj as any).id = id;
+  for (const [key, { foreignKey }] of Object.entries(obj.assosiations)) {
+    const value = obj[key as keyof T];
+    if (Array.isArray(value)) {
+      for (const instance of value) {
+        instance[foreignKey] = id;
+        instance.save();
+      }
+    }
+  }
+  return true;
+};
