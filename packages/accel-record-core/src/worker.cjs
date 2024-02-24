6d02ddb3114a9b53f893b324ea64f39f7477c15f
koyopro <koyopro2.0@gmail.com>
2024-02-25 00:35:11 +0900

    SQL debug log

--- packages/accel-record-core/src/worker.cjs
+++ packages/accel-record-core/src/worker.cjs
@@ -4,12 +4,29 @@ process.env.DATABASE_URL = `file:./test${process.env.VITEST_POOL_ID}.db`;
 
 const prisma = new PrismaClient();
 
+const DEBUG = false;
+
+const output = (...args) => {
+  if (DEBUG) console.log(...args);
+};
+
+const logger = {
+  debug: output,
+  info: output,
+  error: output,
+};
+
 function init(connection) {
   return function (query) {
     const { type, sql, bindings } = query;
     if (type == "query") {
+      logger.info(`  \x1b[36mSQL  \x1b[34m${sql}\x1b[39m`, bindings);
       return prisma.$queryRawUnsafe(sql, ...bindings);
     } else {
+      const color = /begin|commit|rollback/i.test(sql)
+        ? "\x1b[36m"
+        : "\x1b[32m";
+      logger.info(`  \x1b[36mSQL  ${color}${sql}\x1b[39m`, bindings);
       return prisma.$executeRawUnsafe(sql, ...bindings);
     }
   };
