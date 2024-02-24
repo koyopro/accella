24946e9164a1997bde66e40a60d53dccacb17e54
koyopro <koyopro2.0@gmail.com>
2024-02-25 01:26:17 +0900

    skip-generate on start test

--- packages/accel-record/tests/vitest.setup.ts
+++ packages/accel-record/tests/vitest.setup.ts
@@ -12,7 +12,7 @@ beforeAll(() => {
   process.env.DATABASE_URL = `file:./test${process.env.VITEST_POOL_ID}.db`;
   const __dirname = path.dirname(fileURLToPath(import.meta.url));
   const schemaPath = path.resolve(__dirname, "./prisma/schema.prisma");
-  execSync(`npx prisma migrate dev --schema=${schemaPath}`, {
+  execSync(`npx prisma migrate dev --schema=${schemaPath} --skip-generate`, {
     stdio: "inherit",
   });
 });
