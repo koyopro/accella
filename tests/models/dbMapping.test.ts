3bd14c3929e36b7b4204145119c1520c2de3bcbc
koyopro <koyopro2.0@gmail.com>
2024-03-06 01:36:26 +0900

    test for default value

--- tests/models/dbMapping.test.ts
+++ tests/models/dbMapping.test.ts
@@ -1,10 +1,12 @@
 import { $user } from "../factories/user";
 import { Profile } from "./profile";
-import { User } from "./user";
 
 describe("database mapping", () => {
   test("mapping table name", () => {
     const user = $user.create();
     const p = Profile.create({ user: user });
+    expect(p.point).toBe(100);
+    expect(p.enabled).toBe(true);
+    // expect(p.bio).toBe("I'm a Prisma user!");
   });
 });
