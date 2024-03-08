5bfd99690f796ddb42070c0d56c7fb1a01e6c0f4
koyopro <koyopro2.0@gmail.com>
2024-03-09 02:14:52 +0900

    refactor tests/vitest.setup.ts

--- tests/vitest.setup.ts
+++ tests/vitest.setup.ts
@@ -1,4 +1,3 @@
-import { Prisma } from "@prisma/client";
 import { initAccelRecord, Model, stopWorker } from "accel-record";
 import { execSync } from "child_process";
 import path from "path";
@@ -6,39 +5,41 @@ import { fileURLToPath } from "url";
 
 import "./models/index.js";
 
-const getPrismaClientConfig = () => {
-  const ret = {} as Prisma.PrismaClientOptions;
-  if (process.env.VITEST_POOL_ID) {
-    ret.datasourceUrl =
-      process.env.DB_ENGINE == "mysql"
-        ? `mysql://root:@localhost:3306/accel_test${process.env.VITEST_POOL_ID}`
-        : `file:./test${process.env.VITEST_POOL_ID}.db`;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const dbConfig = (type: "mysql" | "sqlite") => {
+  if (type == "mysql") {
+    const connection = `mysql://root:@localhost:3306/accel_test${process.env.VITEST_POOL_ID}`;
+    return {
+      type: "mysql",
+      datasourceUrl: connection,
+      knexConfig: {
+        client: "mysql2",
+        connection,
+      },
+    } as const;
+  } else {
+    const connection = path.resolve(
+      __dirname,
+      `./prisma/test${process.env.VITEST_POOL_ID}.db`
+    );
+    return {
+      type: "sqlite",
+      datasourceUrl: `file:${connection}`,
+      knexConfig: {
+        client: "better-sqlite3",
+        useNullAsDefault: true,
+        connection,
+      },
+    } as const;
   }
-  return ret;
 };
 
 beforeAll(() => {
-  const __dirname = path.dirname(fileURLToPath(import.meta.url));
   const type = process.env.DB_ENGINE == "mysql" ? "mysql" : "sqlite";
-  const knexConfig =
-    type == "mysql"
-      ? {
-          client: "mysql2",
-          connection: `mysql://root:@localhost:3306/accel_test${process.env.VITEST_POOL_ID}`,
-        }
-      : {
-          client: "better-sqlite3",
-          useNullAsDefault: true,
-          connection: {
-            filename: path.resolve(
-              __dirname,
-              `./prisma/test${process.env.VITEST_POOL_ID}.db`
-            ),
-          },
-        };
-  initAccelRecord({ type, knexConfig });
-
-  process.env.DATABASE_URL = getPrismaClientConfig().datasourceUrl;
+  const config = dbConfig(type);
+  initAccelRecord(config);
+  process.env.DATABASE_URL = config.datasourceUrl;
   const schemaDir = type == "mysql" ? "prisma_mysql" : "prisma";
   const schemaPath = path.resolve(__dirname, `./${schemaDir}/schema.prisma`);
   execSync(`npx prisma migrate dev --schema=${schemaPath} --skip-generate`, {
