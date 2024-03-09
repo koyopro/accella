6f9fa47767ea846caae07acf2fd5a463f42a4f07
koyopro <koyopro2.0@gmail.com>
2024-03-10 01:41:39 +0900

    execAllPendingMigrations

--- tests/vitest.setup.ts
+++ tests/vitest.setup.ts
@@ -2,8 +2,12 @@ import { initAccelRecord, Model, stopWorker } from "accel-record";
 import { execSync } from "child_process";
 import path from "path";
 import { fileURLToPath } from "url";
+import fs from "fs";
+import crypto from "crypto";
 
 import "./models/index.js";
+import { getKnex } from "accel-record-core/src/database.js";
+import { log } from "console";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -35,16 +39,71 @@ const dbConfig = (type: "mysql" | "sqlite") => {
   }
 };
 
-beforeAll(() => {
+const sha256hashSync = (buffer: Buffer) => {
+  return crypto.createHash("sha256").update(buffer).digest("hex");
+};
+
+const execAllPendingMigrations = async (type: "mysql" | "sqlite") => {
+  const schemaDir = type == "mysql" ? "prisma_mysql" : "prisma";
+  const migrationsPath = path.resolve(__dirname, `./${schemaDir}/migrations`);
+  const knex = getKnex();
+  const logsTable = "_prisma_migrations";
+  await knex.raw(`CREATE TABLE IF NOT EXISTS "_prisma_migrations" (
+    "id"                    TEXT PRIMARY KEY NOT NULL,
+    "checksum"              TEXT NOT NULL,
+    "finished_at"           DATETIME,
+    "migration_name"        TEXT NOT NULL,
+    "logs"                  TEXT,
+    "rolled_back_at"        DATETIME,
+    "started_at"            DATETIME NOT NULL DEFAULT current_timestamp,
+    "applied_steps_count"   INTEGER UNSIGNED NOT NULL DEFAULT 0
+);`);
+  const logs = await knex(logsTable).select("*");
+  const logsMap = new Map(logs.map((log) => [log.migration_name, log]));
+  let applyCount = 0;
+  for (const dir of fs.readdirSync(migrationsPath)) {
+    const sqlPath = path.resolve(
+      __dirname,
+      `./${schemaDir}/migrations/${dir}/migration.sql`
+    );
+    if (!fs.existsSync(sqlPath)) continue;
+    const buffer = fs.readFileSync(sqlPath);
+    if (logsMap.get(dir)) continue;
+    const startedAt = new Date();
+    console.log(`Applying migration \`${dir}\``);
+    for (const statement of buffer.toString().split(";")) {
+      if (statement.trim() == "") continue;
+      await knex.raw(statement);
+    }
+    const digest = sha256hashSync(buffer);
+    await knex(logsTable).insert({
+      id: crypto.randomUUID(),
+      migration_name: dir,
+      checksum: digest,
+      started_at: startedAt,
+      finished_at: new Date(),
+      applied_steps_count: 1,
+    });
+    applyCount++;
+  }
+  if (applyCount == 0) {
+    console.log(
+      "Already in sync, no schema change or pending migration was found."
+    );
+  }
+};
+
+beforeAll(async () => {
   const type = process.env.DB_ENGINE == "mysql" ? "mysql" : "sqlite";
   const config = dbConfig(type);
   initAccelRecord(config);
-  process.env.DATABASE_URL = config.datasourceUrl;
-  const schemaDir = type == "mysql" ? "prisma_mysql" : "prisma";
-  const schemaPath = path.resolve(__dirname, `./${schemaDir}/schema.prisma`);
-  execSync(`npx prisma migrate dev --schema=${schemaPath} --skip-generate`, {
-    stdio: "inherit",
-  });
+  await execAllPendingMigrations(type);
+  // process.env.DATABASE_URL = config.datasourceUrl;
+  // const schemaDir = type == "mysql" ? "prisma_mysql" : "prisma";
+  // const schemaPath = path.resolve(__dirname, `./${schemaDir}/schema.prisma`);
+  // execSync(`npx prisma migrate dev --schema=${schemaPath} --skip-generate`, {
+  //   stdio: "inherit",
+  // });
 });
 
 beforeEach(async () => {
