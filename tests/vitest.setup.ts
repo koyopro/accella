1512d03b59e3caeadf34625298661a1a7f8e4285
koyopro <koyopro2.0@gmail.com>
2024-03-10 02:44:15 +0900

    refactor vitest.setup.ts

--- tests/vitest.setup.ts
+++ tests/vitest.setup.ts
@@ -9,29 +9,25 @@ export const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const dbConfig = () => {
   if (process.env.DB_ENGINE == "mysql") {
-    const connection = `mysql://root:@localhost:3306/accel_test${process.env.VITEST_POOL_ID}`;
     return {
       type: "mysql",
-      datasourceUrl: connection,
       prismaDir: path.resolve(__dirname, "./prisma_mysql"),
       knexConfig: {
         client: "mysql2",
-        connection,
+        connection: `mysql://root:@localhost:3306/accel_test${process.env.VITEST_POOL_ID}`,
       },
     } as const;
   } else {
-    const connection = path.resolve(
-      __dirname,
-      `./prisma/test${process.env.VITEST_POOL_ID}.db`
-    );
     return {
       type: "sqlite",
-      datasourceUrl: `file:${connection}`,
       prismaDir: path.resolve(__dirname, "./prisma"),
       knexConfig: {
         client: "better-sqlite3",
         useNullAsDefault: true,
-        connection,
+        connection: path.resolve(
+          __dirname,
+          `./prisma/test${process.env.VITEST_POOL_ID}.db`
+        ),
       },
     } as const;
   }
