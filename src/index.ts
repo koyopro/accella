fc1e31008f837309e3747f3fb954d40826e93d55
koyopro <koyopro2.0@gmail.com>
2024-03-13 02:04:44 +0900

    src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+import { initAccelRecord } from "accel-record";
 import { User } from "../tests/models/index.js";
 
-const u = User.build({});
+initAccelRecord({
+  type: "mysql",
+  knexConfig: {
+    client: "mysql2",
+    connection: {
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "",
+      database: `accel_test`,
+    },
+  },
+}).then(() => {
+  User.create({ email: "a4" });
+});
