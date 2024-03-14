3e9d715c829a23da16153311d6032b1e6557b282
koyopro <koyopro2.0@gmail.com>
2024-03-15 01:19:01 +0900

    test for belongsTo association types

---
+++ tests/models/belongsTo.test-d.ts
@@ -0,0 +1,50 @@
+import { NewSetting, NewUser, Setting, User } from ".";
+import { $setting } from "../factories/setting";
+import { $user } from "../factories/user";
+
+describe("getter/setter types", () => {
+  test("new & new", () => {
+    const setting = $setting.build();
+    expectTypeOf(setting).toMatchTypeOf<NewSetting>();
+
+    const user = $user.build();
+    expectTypeOf(user).toMatchTypeOf<NewUser>();
+
+    // @ts-expect-error
+    assertType((setting.user = user));
+  });
+
+  test("new & persisted", () => {
+    const setting = $setting.build();
+    expectTypeOf(setting).toMatchTypeOf<NewSetting>();
+
+    const user = $user.create();
+    expectTypeOf(user).toMatchTypeOf<User>();
+
+    expectTypeOf(setting.user).toMatchTypeOf<User | undefined>();
+    assertType((setting.user = user));
+  });
+
+  test("persisted & new", () => {
+    const setting = $setting.create();
+    expectTypeOf(setting).toMatchTypeOf<Setting>();
+
+    const user = $user.build();
+    expectTypeOf(user).toMatchTypeOf<NewUser>();
+
+    expectTypeOf(setting.user).toMatchTypeOf<User>();
+    // @ts-expect-error
+    assertType((setting.user = user));
+  });
+
+  test("persisted & persisted", () => {
+    const setting = $setting.create();
+    expectTypeOf(setting).toMatchTypeOf<Setting>();
+
+    const user = $user.create();
+    expectTypeOf(user).toMatchTypeOf<User>();
+
+    expectTypeOf(setting.user).toMatchTypeOf<User>();
+    assertType((setting.user = user));
+  });
+});
