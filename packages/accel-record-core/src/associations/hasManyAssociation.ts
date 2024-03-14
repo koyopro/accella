4965063759ce2980908409addeeb30519d135ef5
koyopro <koyopro2.0@gmail.com>
2024-03-15 02:30:13 +0900

    refactor HasManyAssociation

--- packages/accel-record-core/src/associations/hasManyAssociation.ts
+++ packages/accel-record-core/src/associations/hasManyAssociation.ts
@@ -4,13 +4,7 @@ import { Association } from "./association";
 // cf. https://github.com/rails/rails/blob/main/activerecord/lib/active_record/associations/has_many_association.rb#L11
 export class HasManyAssociation<T extends Model> extends Association<T> {
   concat(records: T | T[]) {
-    const _records = Array.isArray(records) ? records : [records];
-    for (const record of _records) {
-      Object.assign(record, this.scopeAttributes());
-      if (this.owner.isPersisted()) {
-        record.save();
-      }
-    }
+    this.persist(records);
   }
 
   persist(records: T | T[]) {
--- packages/accel-record-core/src/associations/hasManyThroughAssociation.ts
+++ packages/accel-record-core/src/associations/hasManyThroughAssociation.ts
@@ -7,16 +7,7 @@ export class HasManyThroughAssociation<
   T extends Model,
 > extends HasManyAssociation<T> {
   override concat(records: T | T[]) {
-    const _records = Array.isArray(records) ? records : [records];
-    for (const record of _records) {
-      if (!this.owner.isPersisted()) continue;
-      record.save();
-      const query = this.connection.knex(this.info.through).insert({
-        ...this.scopeAttributes(),
-        [this.joinKey]: record.pkValues[0],
-      });
-      exec(query);
-    }
+    this.persist(records);
   }
 
   persist(records: T | T[]) {
