dfa3a9df2b9493eb004629f6a7fd3b1f459ab1e3
koyopro <koyopro2.0@gmail.com>
2024-03-16 21:53:37 +0900

    migration

---
+++ tests/prisma_mysql/migrations/20240316093350_add_cuid/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - The required column `cuid` was added to the `profiles` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
+
+*/
+-- AlterTable
+ALTER TABLE `profiles` ADD COLUMN `cuid` VARCHAR(191) NOT NULL,
+    MODIFY `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
