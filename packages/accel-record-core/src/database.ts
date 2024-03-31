3ecb8e07858105ff83d283c389903cba732c88ac
koyopro <koyopro2.0@gmail.com>
2024-04-01 01:26:05 +0900

    custom logger

--- packages/accel-record-core/src/database.ts
+++ packages/accel-record-core/src/database.ts
@@ -6,14 +6,23 @@ import { Model } from "./index.js";
 // @ts-ignore
 import SyncRpc from "./sync-rpc/index.cjs";
 
-const output = (...args: any[]) => {
-  console.log(...args);
+const log = (logLevel: LogLevel, ...args: any[]) => {
+  if (
+    LogLevel.indexOf(logLevel) >= LogLevel.indexOf(_config.logLevel ?? "WARN")
+  ) {
+    const func = (logLevel.toLowerCase() ?? "info") as keyof typeof logger;
+    const _logger = _config.logger ?? logger;
+    _logger[func](...args);
+  }
 };
 
 const logger = {
-  debug: output,
-  info: output,
-  error: output,
+  trace: console.log as (...args: any[]) => any,
+  debug: console.debug as (...args: any[]) => any,
+  info: console.info as (...args: any[]) => any,
+  warn: console.warn as (...args: any[]) => any,
+  error: console.error as (...args: any[]) => any,
+  fatal: console.error as (...args: any[]) => any,
 };
 
 const getKnexConfig = (config: Config) => {
@@ -93,12 +102,11 @@ export const execSQL = (params: {
   const ret = _rpcClient(params);
   const time = Date.now() - startTime;
   const color = /begin|commit|rollback/i.test(sql) ? "\x1b[36m" : "\x1b[32m";
-  if (
-    LogLevel.indexOf(params.logLevel ?? "DEBUG") >=
-    LogLevel.indexOf(_config.logLevel ?? "WARN")
-  ) {
-    logger.info(`  \x1b[36mSQL(${time}ms)  ${color}${sql}\x1b[39m`, bindings);
-  }
+  log(
+    params.logLevel ?? "DEBUG",
+    `  \x1b[36mSQL(${time}ms)  ${color}${sql}\x1b[39m`,
+    bindings
+  );
   return _config.type == "mysql" ? ret[0] : ret;
 };
 
