b8cc0638c05639004493b4bc74b769f429307922
koyopro <koyopro2.0@gmail.com>
2024-05-02 02:53:43 +0900

    refactor

--- packages/accel-record-core/src/model/import.ts
+++ packages/accel-record-core/src/model/import.ts
@@ -1,15 +1,18 @@
+import { Knex } from "knex";
 import { exec } from "../database.js";
 import { Meta, Model } from "../index.js";
+
+type ImportOptions<T extends typeof Model> = {
+  validate?: boolean | "throw";
+  onDuplicateKeyUpdate?: true | (keyof Meta<T>["OrderInput"])[];
+  conflictTarget?: (keyof Meta<T>["OrderInput"])[];
+};
 
 export class Import {
   static import<T extends typeof Model>(
     this: T,
     records: Meta<T>["Base"][] | Meta<T>["CreateInput"][],
-    options: {
-      validate?: boolean | "throw";
-      onDuplicateKeyUpdate?: true | (keyof Meta<T>["OrderInput"])[];
-      conflictTarget?: (keyof Meta<T>["OrderInput"])[];
-    } = {}
+    options: ImportOptions<T> = {}
   ) {
     const _records = records.map((r) =>
       r instanceof Model ? r : this.build(r)
@@ -24,20 +27,29 @@ export class Import {
       })
       .filter(Boolean);
     let q = this.queryBuilder.insert(params);
+    q = this.addOnConflictMerge<T>(options, q);
+    exec(q);
+  }
+
+  private static addOnConflictMerge<T extends typeof Model>(
+    this: T,
+    options: ImportOptions<T>,
+    q: Knex.QueryBuilder
+  ) {
     if (options.onDuplicateKeyUpdate) {
       const attributes = options.onDuplicateKeyUpdate;
-      const q1 = Array.isArray(options.conflictTarget)
+      const qb = Array.isArray(options.conflictTarget)
         ? q.onConflict(options.conflictTarget)
         : q.onConflict();
-      if (attributes === true) q = q1.merge();
+      if (attributes === true) q = qb.merge();
       else {
         const columns = attributes.map(
           (a) => this.attributeToColumn(a as string)!
         );
-        q = q1.merge(columns);
+        q = qb.merge(columns);
       }
     }
-    exec(q);
+    return q;
   }
 
   private static makeInsertParams(record: any) {
