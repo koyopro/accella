268b777f812a4165390775ee97cfdae3686ede3f
koyopro <koyopro2.0@gmail.com>
2024-05-02 03:14:07 +0900

    docs for .import

--- packages/accel-record-core/src/model/import.ts
+++ packages/accel-record-core/src/model/import.ts
@@ -2,9 +2,30 @@ import { Knex } from "knex";
 import { exec } from "../database.js";
 import { Meta, Model } from "../index.js";
 
+/**
+ * Options for importing data into a model.
+ */
 type ImportOptions<T extends typeof Model> = {
+  /**
+   * Specifies whether to validate the imported data.
+   * - If not provided or set to `true`, the data will be validated.
+   * - If set to `"throw"`, an error will be thrown if validation fails.
+   * - If set to `false`, no validation will be performed.
+   */
   validate?: boolean | "throw";
+
+  /**
+   * Specifies whether to update existing records when a duplicate key is encountered.
+   * - If not provided or set to `true`, existing records will be updated.
+   * - If set to an array of keys, only the specified keys will be updated.
+   */
   onDuplicateKeyUpdate?: true | (keyof Meta<T>["OrderInput"])[];
+
+  /**
+   * Specifies the conflict target for resolving conflicts when a duplicate key is encountered.
+   * - If not provided, the entire record will be considered as the conflict target.
+   * - If set to an array of keys, only the specified keys will be considered as the conflict target.
+   */
   conflictTarget?: (keyof Meta<T>["OrderInput"])[];
 };
 
@@ -13,7 +34,18 @@ type ImportResult<T extends typeof Model> = {
   failedInstances: Meta<T>["Base"][];
 };
 
+/**
+ * Represents a class for Bulk Import.
+ *
+ * This class is intended to be inherited by the Model class.
+ */
 export class Import {
+  /**
+   * Imports records into the database.
+   * @param records - The records to import.
+   * @param options - The import options.
+   * @returns An object containing the number of inserts and any failed instances.
+   */
   static import<T extends typeof Model>(
     this: T,
     records: Meta<T>["Base"][] | Meta<T>["CreateInput"][],
@@ -44,6 +76,12 @@ export class Import {
     };
   }
 
+  /**
+   * Adds the ON CONFLICT MERGE clause to the query.
+   * @param options - The import options.
+   * @param q - The query builder.
+   * @returns The modified query builder.
+   */
   private static addOnConflictMerge<T extends typeof Model>(
     this: T,
     options: ImportOptions<T>,
@@ -65,6 +103,11 @@ export class Import {
     return q;
   }
 
+  /**
+   * Creates the insert parameters for a record.
+   * @param record - The record.
+   * @returns The insert parameters.
+   */
   private static makeInsertParams(record: any) {
     return record.makeInsertParams();
   }
