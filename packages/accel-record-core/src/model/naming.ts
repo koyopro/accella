c2063d6822792807a9c29582b879cbd9cc0fad21
koyopro <koyopro2.0@gmail.com>
2024-06-11 01:37:15 +0900

    Model.modelName.human

--- packages/accel-record-core/src/model/naming.ts
+++ packages/accel-record-core/src/model/naming.ts
@@ -1,10 +1,17 @@
+import i18next from "i18next";
+
 export class Naming {
   static get modelName() {
     const name = this.name;
+    const key = `accelrecord.models.${toSnakeCase(name)}`;
     return {
       get human() {
-        return name;
+        return i18next.t(key, name) ?? name;
       },
     };
   }
 }
+
+const toSnakeCase = (str: string) => {
+  return str.replace(/[A-Z]/g, (s) => "_" + s.toLowerCase()).replace(/^_/, "");
+};
--- tests/models/model/naming.test.ts
+++ tests/models/model/naming.test.ts
@@ -1,6 +1,26 @@
+import i18next from "i18next";
 import { Setting, User } from "..";
 
-test("modelName.human", () => {
+test("modelName.human", async () => {
   expect(User.modelName.human).toBe("User");
+  expect(Setting.modelName.human).toBe("Setting");
+});
+
+test("modelName.human with i18n", async () => {
+  await i18next.init({
+    lng: "ja",
+    resources: {
+      ja: {
+        translation: {
+          accelrecord: {
+            models: {
+              user: "ユーザー",
+            },
+          },
+        },
+      },
+    },
+  });
+  expect(User.modelName.human).toBe("ユーザー");
   expect(Setting.modelName.human).toBe("Setting");
 });
