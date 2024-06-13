3363acebd18b860c4ef6a5798076d46e9c3a136f
koyopro <koyopro2.0@gmail.com>
2024-06-14 01:47:07 +0900

    docs for Naming

--- packages/accel-record-core/src/model/naming.ts
+++ packages/accel-record-core/src/model/naming.ts
@@ -3,17 +3,34 @@ type i18n = {
 };
 export let i18n: i18n | undefined = undefined;
 
+/**
+ * Represents a class for handling naming conventions.
+ *
+ * This class is intended to be inherited by the Model class.
+ */
 export class Naming {
+  /**
+   * Gets the model name.
+   * @returns An object with a `human` property representing the human-readable model name.
+   */
   static get modelName() {
     const default_ = this.name;
     const key = `accelrecord.models.${this.name}`;
     return {
+      /**
+       * the human-readable model name.
+       */
       get human() {
         return i18n?.t(key, "") || default_;
       },
     };
   }
 
+  /**
+   * Gets the human-readable attribute name for the specified attribute.
+   * @param attribute - The attribute name.
+   * @returns The human-readable attribute name.
+   */
   static humanAttributeName(attribute: string) {
     const key = `accelrecord.attributes.${this.name}.${attribute}`;
     return i18n?.t(key, "") || toPascalCase(attribute);
