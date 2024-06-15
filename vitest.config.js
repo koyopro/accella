71a44dd09598db496754ac2b662a049f04b4378c
koyopro <koyopro2.0@gmail.com>
2024-06-15 23:47:02 +0900

    config esbuild target es2022
    for decorators

--- vitest.config.js
+++ vitest.config.js
@@ -1,13 +1,17 @@
 import path from "path";
+import { defineConfig } from "vitest/config";
 
-export default {
+export default defineConfig({
   test: {
     globals: true,
     setupFiles: ["./tests/vitest.setup.ts"],
+  },
+  esbuild: {
+    target: "es2022",
   },
   resolve: {
     alias: {
       src: path.resolve(__dirname, "./src"),
     },
   },
-};
+});
