93fb4e22bc0010540f5fd0dfd0c2b13397f288ac
koyopro <koyopro2.0@gmail.com>
2024-06-17 01:49:55 +0900

    refactor

--- packages/accel-record-core/src/callbacks.ts
+++ packages/accel-record-core/src/callbacks.ts
@@ -1,26 +1,20 @@
 import { Model } from "./index.js";
 
-const methodsForBeforeCallback = [
+const callbackMethods = [
   "validation",
   "create",
   "save",
   "update",
   "destroy",
 ] as const;
-const methodsForAfterCallback = [
-  "validation",
-  "create",
-  "save",
-  "update",
-  "destroy",
-] as const;
+type CallbackMethod = (typeof callbackMethods)[number];
 
 /**
  * The `before` decorator registers a callback function to be executed before the specified method is called.
  *
  * @param method - The name of the method to add the callback to.
  */
-export const before = (method: (typeof methodsForBeforeCallback)[number]) => {
+export const before = (method: CallbackMethod) => {
   return function (originalMethod: (...args: any[]) => any, context: any) {
     context.addInitializer(function (this: Model) {
       this.callbacks.before[method].push(originalMethod);
@@ -34,7 +28,7 @@ export const before = (method: (typeof methodsForBeforeCallback)[number]) => {
  *
  * @param method - The name of the method to add the callback to.
  */
-export const after = (method: (typeof methodsForAfterCallback)[number]) => {
+export const after = (method: CallbackMethod) => {
   return function (originalMethod: (...args: any[]) => any, context: any) {
     context.addInitializer(function (this: Model) {
       this.callbacks.after[method].push(originalMethod);
@@ -53,17 +47,15 @@ export class Callbacks {
    * An object that stores the callbacks.
    */
   callbacks = {
-    before: methodsForBeforeCallback.toHash((m) => [m, [] as Function[]]),
-    after: methodsForAfterCallback.toHash((m) => [m, [] as Function[]]),
+    before: callbackMethods.toHash((m) => [m, [] as Function[]]),
+    after: callbackMethods.toHash((m) => [m, [] as Function[]]),
   };
 
   /**
    * Runs the before callbacks for a given method.
    * @param method - The method for which to run the before callbacks.
    */
-  protected runBeforeCallbacks(
-    method: (typeof methodsForBeforeCallback)[number]
-  ) {
+  protected runBeforeCallbacks(method: CallbackMethod) {
     for (const cb of this.callbacks.before[method]) {
       cb.call(this);
     }
@@ -73,9 +65,7 @@ export class Callbacks {
    * Runs the after callbacks for a given method.
    * @param method - The method for which to run the after callbacks.
    */
-  protected runAfterCallbacks(
-    method: (typeof methodsForAfterCallback)[number]
-  ) {
+  protected runAfterCallbacks(method: CallbackMethod) {
     for (const cb of this.callbacks.after[method]) {
       cb.call(this);
     }
