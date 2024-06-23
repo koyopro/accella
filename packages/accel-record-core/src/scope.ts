1bdc793a946a4e6238c00f13868765339ceae3f2
koyopro <koyopro2.0@gmail.com>
2024-06-24 05:00:55 +0900

    fix error

--- packages/accel-record-core/src/scope.ts
+++ packages/accel-record-core/src/scope.ts
@@ -1,6 +1,9 @@
 import { Relation } from "./relation/index.js";
 
-export function scope(method: (...args: any[]) => Relation<any, any>) {
+export function scope(
+  method: (...args: any[]) => Relation<any, any>,
+  context: any
+) {
   (method as any).isAccelRecordScope = true;
   return method;
 }
