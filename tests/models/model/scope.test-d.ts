18c045f7e727f18f4d87966bdc0e96c6e490fb72
koyopro <koyopro2.0@gmail.com>
2024-06-24 04:49:06 +0900

    test

--- tests/models/model/scope.test-d.ts
+++ tests/models/model/scope.test-d.ts
@@ -1,19 +1,16 @@
-import { User } from "..";
+import { Post, User } from "..";
 
 test("scope", () => {
   User.john();
   User.all().john().adults().count();
   User.adults().john().count();
-  User.all()
-    .includes()
-    .joins()
-    .joinsRaw("")
-    .order("name")
-    .offset(0)
-    .limit(0)
-    .where({})
-    .whereNot({})
-    .whereRaw("")
-    .adults()
-    .count();
+  // @ts-expect-error
+  User.select("id").john();
+
+  Post.john();
+  Post.all().john();
+  // @ts-expect-error
+  Post.adults();
+  // @ts-expect-error
+  Post.all().adults();
 });
