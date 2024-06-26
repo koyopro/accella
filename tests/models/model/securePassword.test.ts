23115922a9f1f45df00d40497d83a470926ad5a7
koyopro <koyopro2.0@gmail.com>
2024-06-26 16:11:58 +0900

    refactor test

--- tests/models/model/securePassword.test.ts
+++ tests/models/model/securePassword.test.ts
@@ -4,26 +4,30 @@ import { $user } from "../../factories/user";
 import { ApplicationRecord } from "../applicationRecord";
 
 test("hasSecurePassword()", () => {
-  const user = $user.build({ name: "david" });
+  const user = $user.build({});
   user.password = "";
-  user.passwordConfirmation = "nomatch";
-  expect(user.save()).toBe(false); // password required
-  user.password = "mUc3m00RsqyRe";
-  expect(user.save()).toBe(false); // confirmation doesn't match
-  user.passwordConfirmation = "mUc3m00RsqyRe";
+  user.passwordConfirmation = "";
+  expect(user.save()).toBe(false);
+  expect(user.errors.fullMessages).toEqual(["Password can't be blank"]);
+
+  user.password = "xBOHdowK5e2YjQ1s";
+  expect(user.save()).toBe(false);
+  expect(user.errors.fullMessages).toEqual([
+    "PasswordConfirmation does not match password",
+  ]);
+
+  user.passwordConfirmation = "xBOHdowK5e2YjQ1s";
   expect(user.save()).toBe(true);
+  expect(user.errors.fullMessages).toEqual([]);
 
   expect(user.passwordDigest).not.toBeUndefined();
-  // user.recovery_password = "42password"
-  // user.recovery_password_digest                              #=> "$2a$04$iOfhwahFymCs5weB3BNH/uXkTG65HR.qpW.bNhEjFP3ftli3o5DQC"
-  // user.save                                                  #=> true
 
-  expect(user.authenticate("notright")).toBe(false);
-  expect(user.authenticate("mUc3m00RsqyRe")).toBe(true);
+  expect(user.authenticate("invalid")).toBe(false);
+  expect(user.authenticate("xBOHdowK5e2YjQ1s")).toBe(true);
 
   const u = User.find(user.id!);
-  expect(u.authenticate("notright")).toBe(false);
-  expect(u.authenticate("mUc3m00RsqyRe")).toBe(true);
+  expect(u.authenticate("invalid")).toBe(false);
+  expect(u.authenticate("xBOHdowK5e2YjQ1s")).toBe(true);
 
   // @ts-expect-error
   user.hoge;
@@ -45,6 +49,6 @@ test("hasSecurePassword() with custom attribute", () => {
     hasSecurePassword({ attribute: "recovery" })
   ) {}
   const user = new CustomAttributeUser();
-  user.recovery = user.recoveryConfirmation = "mUc3m00RsqyRe";
+  user.recovery = user.recoveryConfirmation = "xBOHdowK5e2YjQ1s";
   expect(user.isValid()).toBe(true);
 });
