ce58963427c462296413afa11000469a3f60472c
koyopro <koyopro2.0@gmail.com>
2024-07-19 01:47:39 +0900

    fix test

--- tests/models/model/scope.test.ts
+++ tests/models/model/scope.test.ts
@@ -2,8 +2,8 @@ import { User } from "..";
 import { $user } from "../../factories/user";
 
 test("scope", () => {
-  $user.create({ name: "john", age: 19 });
-  $user.create({ name: "john", age: 20 });
+  $user.create({ name: "John", age: 19 });
+  $user.create({ name: "John", age: 20 });
 
   expect(User.john().count()).toBe(2);
   expect(User.john().adults().count()).toBe(1);
