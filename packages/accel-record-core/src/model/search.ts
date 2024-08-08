912e9f98ab7a92b9f303aece02f399bc066d84a5
koyopro <koyopro2.0@gmail.com>
2024-08-09 01:45:00 +0900

    refactor

--- packages/accel-record-core/src/model/search.ts
+++ packages/accel-record-core/src/model/search.ts
@@ -1,15 +1,66 @@
-import { a } from "vitest/dist/suite-xGC-mxBC.js";
 import { Model, Relation } from "../index.js";
 
 export class Searchable {
-  static search<T extends typeof Model>(
-    this: T,
-    params: Record<string, number | string>
-  ) {
+  static search<T extends typeof Model>(this: T, params: Record<string, any>) {
     return new Search(this, params);
   }
 
   static searchableScopes: string[] = [];
+}
+
+export class Predicate {
+  name: string = "";
+  not = false;
+  type: "all" | "any" | undefined;
+
+  constructor(protected key: string) {
+    let name = key;
+    if (name.startsWith("not_")) {
+      name = name.substring(4);
+      this.not = true;
+    }
+    if (name.startsWith("does_not_")) {
+      name = name.substring(9);
+      this.not = true;
+    }
+    if (name.endsWith("_all")) {
+      name = name.substring(0, name.length - 4);
+      this.type = "all";
+    } else if (name.endsWith("_any")) {
+      name = name.substring(0, name.length - 4);
+      this.type = "any";
+    }
+    this.name = name;
+  }
+}
+
+export class Query {
+  name: string = "";
+  predicate: Predicate = new Predicate("");
+
+  constructor(
+    protected key: string,
+    protected value: any
+  ) {
+    const parsed = key.match(
+      /^(.+?)_(((does_)?not_)?(eq|in|cont|start|end|null|match(es)?|lt|lte|gt|gte|true|false|blank|present)(_all|_any)?)$/
+    );
+    if (!parsed) return;
+    this.name = parsed[1];
+    this.predicate = new Predicate(parsed[2]);
+  }
+
+  get isValid() {
+    return this.name !== "" && this.predicate.name !== "";
+  }
+
+  get orList() {
+    return this.name.split("_or_");
+  }
+
+  get attributes() {
+    return this.name.split("_and_");
+  }
 }
 
 export class Search {
@@ -34,52 +85,30 @@ export class Search {
     if (this.model.searchableScopes.includes(key)) {
       return relation.merge((this.model as any)[key](value));
     }
-    const result = key.match(
-      /^(.+?)_(((does_)?not_)?(eq|in|cont|start|end|null|match(es)?|lt|lte|gt|gte|true|false|blank|present)(_all|_any)?)$/
-    );
-    if (!result) return relation;
-    const [name, predicate] = result.slice(1);
-    const ors = name.split("_or_");
-    if (ors.length > 1) {
-      let r = this.buildRelation(this.model.all(), ors[0], predicate, value);
-      for (const attr of ors.slice(1)) {
-        r = r.or(this.buildRelation(this.model.all(), attr, predicate, value));
-      }
-      return relation.merge(r);
+    const q = new Query(key, value);
+    if (!q.isValid) return relation;
+
+    const { predicate, orList } = q;
+    if (orList.length > 1) {
+      let tmp = orList.reduce((acc: Relation<any, any> | undefined, attr) => {
+        const r = this.affectQuery(this.model.all(), attr, predicate, value);
+        return acc?.or(r) ?? r;
+      }, undefined);
+      return relation.merge(tmp!);
     }
-    const attrs = name.split("_and_");
-    for (const attr of attrs) {
-      relation = this.buildRelation(relation, attr, predicate, value);
+    for (const attr of q.attributes) {
+      relation = this.affectQuery(relation, attr, predicate, value);
     }
     return relation;
   }
 
-  private buildRelation(
+  private affectQuery(
     relation: Relation<any, any>,
     attrStr: string,
-    predicate: string,
+    predicate: Predicate,
     value: any
   ) {
-    let not = false;
-    let all = false;
-    let any = false;
-    if (predicate.startsWith("not_")) {
-      predicate = predicate.substring(4);
-      not = true;
-    }
-    if (predicate.startsWith("does_not_")) {
-      predicate = predicate.substring(9);
-      not = true;
-    }
-    if (predicate.endsWith("_all")) {
-      predicate = predicate.substring(0, predicate.length - 4);
-      all = true;
-    }
-    if (predicate.endsWith("_any")) {
-      predicate = predicate.substring(0, predicate.length - 4);
-      any = true;
-    }
-    switch (predicate) {
+    switch (predicate.name) {
       case "blank": {
         const w1 = this.buildWhere(this.model, attrStr, predicate, "");
         const w2 = this.buildWhere(this.model, attrStr, predicate, null);
@@ -90,30 +119,39 @@ export class Search {
         const w2 = this.buildWhere(this.model, attrStr, predicate, null);
         return relation.joins(w1.joins).whereNot(w1.where).whereNot(w2.where);
       }
-      default: {
+      default:
+        return this.affectDefaultQuery(relation, attrStr, predicate, value);
+    }
+  }
+
+  private affectDefaultQuery(
+    relation: Relation<any, any>,
+    attrStr: string,
+    predicate: Predicate,
+    value: any
+  ) {
+    const method = predicate.not ? "whereNot" : "where";
+    const values = [value].flat();
+    switch (predicate.type) {
+      case "all": {
         let ret = relation;
-        const values = [value].flat();
-        if (all) {
-          for (const v of [value].flat()) {
-            const w = this.buildWhere(this.model, attrStr, predicate, v);
-            const method = not ? "whereNot" : "where";
-            ret = ret.joins(w.joins)[method](w.where);
-          }
-        } else if (any) {
-          const method = not ? "whereNot" : "where";
-          const w = this.buildWhere(this.model, attrStr, predicate, values[0]);
-          let r = this.model.joins(w.joins)[method](w.where);
-          for (const v of values.slice(1)) {
-            const w = this.buildWhere(this.model, attrStr, predicate, v);
-            r = r.or(this.model[method](w.where));
-          }
-          return relation.merge(r);
-        } else {
-          const w = this.buildWhere(this.model, attrStr, predicate, value);
-          const method = not ? "whereNot" : "where";
-          ret = relation.joins(w.joins)[method](w.where);
+        for (const v of values) {
+          const w = this.buildWhere(this.model, attrStr, predicate, v);
+          ret = ret.joins(w.joins)[method](w.where);
         }
         return ret;
+      }
+      case "any": {
+        let tmp = values.reduce((acc: Relation<any, any> | undefined, v) => {
+          const w = this.buildWhere(this.model, attrStr, predicate, v);
+          const r = this.model[method](w.where);
+          return acc?.or(r) ?? r.joins(w.joins);
+        }, undefined);
+        return relation.merge(tmp);
+      }
+      default: {
+        const w = this.buildWhere(this.model, attrStr, predicate, value);
+        return relation.joins(w.joins)[method](w.where);
       }
     }
   }
@@ -121,7 +159,7 @@ export class Search {
   private buildWhere(
     model: typeof Model,
     attrStr: string,
-    predicate: string,
+    predicate: Predicate,
     value: any
   ): { where: object; joins: object } {
     const field = model.findField(attrStr);
@@ -129,23 +167,19 @@ export class Search {
       for (const [key, association] of Object.entries(model.associations)) {
         if (attrStr.startsWith(`${key}_`)) {
           const nextAttr = attrStr.substring(key.length + 1);
-          const next = this.buildWhere(
-            association.model,
-            nextAttr,
-            predicate,
-            value
-          );
-          const where = { [key]: next.where };
-          const joins = { [key]: next.joins };
-          return { where, joins };
+          const { model: nextModel } = association;
+          const next = this.buildWhere(nextModel, nextAttr, predicate, value);
+          return {
+            where: { [key]: next.where },
+            joins: { [key]: next.joins },
+          };
         }
       }
     }
-    const where = {
-      [attrStr]: getCondition(predicate, value),
+    return {
+      where: { [attrStr]: getCondition(predicate.name, value) },
+      joins: {},
     };
-    const joins = {};
-    return { where, joins };
   }
 }
 
