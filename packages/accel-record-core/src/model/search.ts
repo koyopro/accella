b29808123e19a898d8cd424fc895f83da056d90a
koyopro <koyopro2.0@gmail.com>
2024-08-09 02:58:48 +0900

    docs for Search

--- packages/accel-record-core/src/model/search.ts
+++ packages/accel-record-core/src/model/search.ts
@@ -1,10 +1,88 @@
 import { Model, Relation } from "../index.js";
 
+/**
+ * Represents a class for handling search parameters.
+ *
+ * This class is intended to be inherited by the Model class.
+ */
 export class Searchable {
+  /**
+   * Creates a new search instance.
+   * @param params - The search parameters.
+   * @returns A new search instance.
+   * @example
+   * ```ts
+   * const search = User.search({ name_eq: "foo" });
+   * const users = search.result();
+   * ```
+   * @details
+   * The search parameters are used to filter the results of the search. \
+   * The search parameters are specified as key-value pairs. \
+   * The key is the name of the attribute to search for.
+   * The key can include associations. \
+   * The value is the value to search for.
+   *
+   * The search parameters can include the following:
+   * - `*_eq` - The attribute is equal to the value.
+   * - `*_cont` - The attribute contains the value.
+   * - `*_start` - The attribute starts with the value.
+   * - `*_end` - The attribute ends with the value.
+   * - `*_matches` - The attribute matches the value (Use the `LIKE` clause).
+   * - `*_lt` - The attribute is less than the value.
+   * - `*_lte` - The attribute is less than or equal to the value.
+   * - `*_gt` - The attribute is greater than the value.
+   * - `*_gte` - The attribute is greater than or equal to the value.
+   * - `*_in` - The attribute is in the list of values.
+   * - `*_true` - The attribute is true.
+   * - `*_false` - The attribute is false.
+   * - `*_null` - The attribute is null.
+   * - `*_blank` - The attribute is blank.
+   * - `*_present` - The attribute is present.
+   *
+   * The search parameters can also include the following modifiers:
+   * - `not` (`does_not`) - The value must not match.
+   * - `all` - All of the values must match.
+   * - `any` - Any of the values must match.
+   *
+   * The search parameters can also include the following logical operators:
+   * - `and` - The values must match all of the attributes.
+   * - `or` - The values must match any of the attributes.
+   * @example
+   * ```ts
+   * const search = User.search({
+   *   profile_bio_cont: "foo", // with association
+   *   age_not_null: 1,         // with `not` modifier
+   *   email_or_name_cont_any: ["bar", "baz"], // with `or` operator and `any` modifier
+   * });
+   * const users = search.result();
+   * ```
+   * @details
+   * The keys of the search parameters can include the names of the searchable scopes defined in the `searchableScopes` array. \
+   * The values of the search parameters specify the values to search for.
+   * @example
+   * ```ts
+   * class UserModel extends ApplicationRecord {
+   *   static bio_cont(value: string) {
+   *     return this.joins("profile").where({
+   *       profile: { bio: { 'contains': value } },
+   *     });
+   *   }
+   *   static searchableScopes = ["bio_cont"];
+   * }
+   * ```
+   * // The `bio_cont` scope can be used in the search parameters.
+   * ```ts
+   * const search = User.search({ bio_cont: "foo" });
+   * const users = search.result();
+   * ```
+   */
   static search<T extends typeof Model>(this: T, params: Record<string, any>) {
     return new Search(this, params);
   }
 
+  /**
+   * An array of searchable scopes used in the search() method.
+   */
   static searchableScopes: string[] = [];
 }
 
@@ -70,6 +148,9 @@ export class Search {
     protected relation: Relation<any, any> | undefined = undefined
   ) {}
 
+  /**
+   * Retrieves the search result based on the specified parameters.
+   */
   result() {
     let relation = this.relation ?? this.model.all();
     for (const [key, value] of Object.entries(this.params)) {
