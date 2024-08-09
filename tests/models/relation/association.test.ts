d24059eb5488c9551ef4bf29a9b17fe47a998b43
koyopro <koyopro2.0@gmail.com>
2024-08-10 01:37:48 +0900

    improve test

--- tests/models/relation/association.test.ts
+++ tests/models/relation/association.test.ts
@@ -1,10 +1,10 @@
-import { User } from "..";
+import { PostTag, User } from "..";
 import { $post } from "../../factories/post";
 import { $postTag } from "../../factories/postTag";
 import { $user } from "../../factories/user";
 
 test("multi level joins()", () => {
-  const u = $user.create();
+  const u = $user.create({ name: "John" });
   const p = $post.create({ author: u });
   $postTag.create({ posts: [p], name: "tag1" });
 
@@ -14,6 +14,12 @@ test("multi level joins()", () => {
   expect(
     User.joins({ posts: ["tags"] })
       .where("PostTag.name = ?", "tag1")
+      .count()
+  ).toBe(1);
+
+  expect(
+    PostTag.joins({ posts: "author" })
+      .where({ posts: { author: { name: "John" } } })
       .count()
   ).toBe(1);
 });
