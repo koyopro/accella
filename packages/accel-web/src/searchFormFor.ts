065b5139ada84d57835cbb2a00fa36b3bea8b73e
koyopro <koyopro2.0@gmail.com>
2024-08-16 02:20:50 +0900

    ignore type error

--- packages/accel-web/src/searchFormFor.ts
+++ packages/accel-web/src/searchFormFor.ts
@@ -2,5 +2,6 @@ import { Search } from "accel-record/search";
 import { formWith } from "./formWith";
 
 export const searchFormFor = (s: Search) => {
-  return formWith(s, { namespace: "q" });
+  // TODO: fix type error
+  return formWith(s as any, { namespace: "q" });
 };
