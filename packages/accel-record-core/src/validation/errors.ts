bd50b076b02e062b3563886d902d0f0abc875db0
koyopro <koyopro2.0@gmail.com>
2024-08-21 00:54:05 +0900

    change i18n params format

--- packages/accel-record-core/src/validation/errors.ts
+++ packages/accel-record-core/src/validation/errors.ts
@@ -6,19 +6,19 @@ const defaultMessages: Record<string, string | undefined> = {
   accepted: "must be accepted",
   invalid: "is invalid",
   inclusion: "is not included in the list",
-  tooShort: "is too short (minimum is %{count} characters)",
-  tooLong: "is too long (maximum is %{count} characters)",
+  tooShort: "is too short (minimum is {{count}} characters)",
+  tooLong: "is too long (maximum is {{count}} characters)",
   taken: "has already been taken",
-  confirmation: "doesn't match %{attribute}",
+  confirmation: "doesn't match {{attribute}}",
 
   notANumber: "is not a number",
   notAnInteger: "must be an integer",
-  equalTo: "must be equal to %{count}",
-  greaterThan: "must be greater than %{count}",
-  greaterThanOrEqualTo: "must be greater than or equal to %{count}",
-  lessThan: "must be less than %{count}",
-  lessThanOrEqualTo: "must be less than or equal to %{count}",
-  otherThan: "must be other than %{count}",
+  equalTo: "must be equal to {{count}}",
+  greaterThan: "must be greater than {{count}}",
+  greaterThanOrEqualTo: "must be greater than or equal to {{count}}",
+  lessThan: "must be less than {{count}}",
+  lessThanOrEqualTo: "must be less than or equal to {{count}}",
+  otherThan: "must be other than {{count}}",
   odd: "must be odd",
   even: "must be even",
 };
@@ -53,10 +53,14 @@ export class Error {
   get message() {
     let message = this.translatedMessage;
     if (this.options.count != undefined) {
-      message = message.replace("%{count}", this.options.count.toString());
+      for (const key of ["{{count}}", "%{count}"]) {
+        message = message.replace(key, this.options.count.toString());
+      }
     }
-    if (message.includes("%{attribute}")) {
-      message = message.replace("%{attribute}", this.options.attribute || "confirmation");
+    if (message.includes("{attribute}")) {
+      for (const key of ["{{attribute}}", "%{attribute}"]) {
+        message = message.replace(key, this.options.attribute || "confirmation");
+      }
     }
     return message;
   }
