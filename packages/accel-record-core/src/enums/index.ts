edb4227aaa39a3a2832f222171e802f2ca3abf65
koyopro <koyopro2.0@gmail.com>
2024-08-21 16:01:44 +0900

    docs for enum

--- packages/accel-record-core/src/enums/index.ts
+++ packages/accel-record-core/src/enums/index.ts
@@ -1,6 +1,9 @@
 import { Model } from "../index.js";
 import { i18n } from "../model/naming.js";
 
+/**
+ * Represents an enum attribute.
+ */
 export class Attribute {
   protected _values: Value[];
 
@@ -12,15 +15,29 @@ export class Attribute {
     this._values = Object.keys(this.enums).map((key) => new Value(this, key));
   }
 
+  /**
+   * Returns the values of the enum.
+   *
+   * @returns An array containing the values of the enum.
+   */
   values() {
     return this._values;
   }
 
-  options() {
+  /**
+   * Retrieves an array of options.
+   * Each option is represented as an array with two elements: the localized text and value.
+   *
+   * @returns The array of options.
+   */
+  options(): [string, string][] {
     return this.values().map((v) => [v.text, v.value]);
   }
 }
 
+/**
+ * Represents a value in an enumeration.
+ */
 export class Value {
   value: string;
 
@@ -31,6 +48,18 @@ export class Value {
     this.value = name;
   }
 
+  /**
+   * Retrieves the localized text for the current enum value.
+   * 
+   * The text is retrieved using the following keys in order:
+   * - `enums.${klass}.${attribute}.${name}`
+   * - `enums.defaults.${attribute}.${name}`
+   * - `enums.${attribute}.${name}`
+   * 
+   * If no localized text is found, the name of the enum value is returned.
+   * 
+   * @returns The localized text for the enum value, or the name if no text is found.
+   */
   get text() {
     const keys = [
       `enums.${this.attribute.klass.name}.${this.attribute.name}.${this.name}`,
