d44051f82ce6c6a12ba2513e0200bcbdb2253c00
koyopro <koyopro2.0@gmail.com>
2024-09-11 04:31:13 +0900

    refactor Parameters

--- packages/accel-web/src/parameters.ts
+++ packages/accel-web/src/parameters.ts
@@ -1,21 +1,14 @@
 import { formDataToObject } from "astro/actions/runtime/virtual/server.js";
 import { z } from "astro/zod";
-import { parseFormData } from "parse-nested-form-data";
 
 export class Parameters {
-  data: ReturnType<typeof parseFormData>;
-  formData: FormData;
-
-  constructor(data: FormData) {
-    this.data = parseFormData(data);
-    this.formData = data;
-  }
+  constructor(protected data: FormData) {}
 
   permit<T extends string[]>(...keys: T) {
     return this.parse(z.object(keys.toHash((key) => [key, z.string()])));
   }
 
   parse(z: any) {
-    return formDataToObject(this.formData, z);
+    return formDataToObject(this.data, z);
   }
 }
