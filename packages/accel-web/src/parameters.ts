c5a6b0726072e5331db233470c15f649676e4c44
koyopro <koyopro2.0@gmail.com>
2024-09-18 00:53:06 +0900

    Revert "refactor Parameters"
    This reverts commit d44051f82ce6c6a12ba2513e0200bcbdb2253c00.

--- packages/accel-web/src/parameters.ts
+++ packages/accel-web/src/parameters.ts
@@ -1,14 +1,21 @@
 import { formDataToObject } from "astro/actions/runtime/virtual/server.js";
 import { z } from "astro/zod";
+import { parseFormData } from "parse-nested-form-data";
 
 export class Parameters {
-  constructor(protected data: FormData) {}
+  data: ReturnType<typeof parseFormData>;
+  formData: FormData;
+
+  constructor(data: FormData) {
+    this.data = parseFormData(data);
+    this.formData = data;
+  }
 
   permit<T extends string[]>(...keys: T) {
     return this.parse(z.object(keys.toHash((key) => [key, z.string()])));
   }
 
   parse(z: any) {
-    return formDataToObject(this.data, z);
+    return formDataToObject(this.formData, z);
   }
 }
