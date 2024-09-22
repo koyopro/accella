be201fa7590274c2448f4f492710e8b7e9a69b95
koyopro <koyopro2.0@gmail.com>
2024-09-23 02:36:05 +0900

    [examples/web_astro] setup I18n

--- examples/web_astro/src/middleware.ts
+++ examples/web_astro/src/middleware.ts
@@ -1,6 +1,8 @@
 import { defineMiddleware } from "astro:middleware";
 import { initDatabase } from "./core/database.js";
+import { initI18n } from "./core/i18n.js";
 
+await initI18n();
 await initDatabase();
 
 export const onRequest = defineMiddleware((_context, next) => {
