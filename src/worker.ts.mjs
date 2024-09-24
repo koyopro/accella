4c85789355dd22f68978051c4ca61cf16d51d6bb
koyopro <koyopro2.0@gmail.com>
2024-09-25 01:59:50 +0900

    defineRpcSyncActions

--- src/worker.ts.mjs
+++ src/worker.ts.mjs
@@ -1,21 +1,20 @@
 // src/worker.ts
-function init() {
-  return async function(params) {
-    const { method, args } = params || {};
-    const action = actions[method];
-    if (action) {
-      return action(...args || []);
-    }
-    return "from mjs worker";
+var defineRpcSyncActions = (actions) => {
+  return () => {
+    return async function(params) {
+      const { method, args } = params || {};
+      const action = actions[method];
+      if (action) {
+        return action(...args || []);
+      }
+      return "from mjs worker";
+    };
   };
-}
-var actions = {
-  incr: async (a) => a + 1,
-  ping: () => "pong!"
 };
-var worker_default = init;
+var worker_default = defineRpcSyncActions({
+  incr: async (a) => a + 1,
+  ping: () => "pong!!"
+});
 export {
-  actions,
-  worker_default as default,
-  init
+  worker_default as default
 };
