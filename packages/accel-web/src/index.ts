38ab61c5f3c000fffacd279489513fb6ed41f637
koyopro <koyopro2.0@gmail.com>
2024-10-12 01:28:23 +0900

    [accel-web] impl Session

--- packages/accel-web/src/index.ts
+++ packages/accel-web/src/index.ts
@@ -1,3 +1,4 @@
 export { formFor } from "./form/index.js";
 export { paginate } from "./paginate/index.js";
 export { RequestParameters } from "./parameters.js";
+export { createCookieSessionStorage } from "./session.js";
