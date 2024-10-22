d40c778829e587c39c6d3f633c61aed8eeec26e7
koyopro <koyopro2.0@gmail.com>
2024-10-23 04:38:34 +0900

    [examples/web_astro] prepare Session

--- examples/web_astro/src/middleware.ts
+++ examples/web_astro/src/middleware.ts
@@ -1,10 +1,18 @@
 import { defineMiddleware } from "astro:middleware";
 import { initDatabase } from "./core/database.js";
 import { initI18n } from "./core/i18n.js";
+import { getSession } from "./session.js";
 
 await initI18n();
 await initDatabase();
 
-export const onRequest = defineMiddleware((_context, next) => {
+export const onRequest = defineMiddleware(({ locals, cookies, request, redirect }, next) => {
+  locals.session = getSession(cookies);
+
+  const path = new URL(request.url).pathname;
+  if (!locals.session.account && !path.startsWith("/sign")) {
+    return redirect("/signin");
+  }
+
   next();
 });
--- packages/accel-web/src/index.ts
+++ packages/accel-web/src/index.ts
@@ -1,4 +1,4 @@
 export { formFor } from "./form/index.js";
 export { paginate } from "./paginate/index.js";
 export { RequestParameters } from "./parameters.js";
-export { createCookieSessionStorage } from "./session.js";
+export { Session, createCookieSessionStorage } from "./session.js";
