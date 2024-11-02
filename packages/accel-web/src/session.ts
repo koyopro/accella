18a86958e3549ca973d56ee2cb8dcc7de3bc1e46
koyopro <koyopro2.0@gmail.com>
2024-11-02 22:57:55 +0900

    [accel-web] docs

--- packages/accel-web/src/session.ts
+++ packages/accel-web/src/session.ts
@@ -35,11 +35,76 @@ export class Session<T, F> extends Base<T, F> {
   }
 }
 
+/**
+ * Creates a cookie session storage.
+ *
+ * @example Standard usage. Specify the type of the session.
+ * ```ts
+ * type SessionData = {
+ *   userId: string;
+ * };
+ *
+ * export const { getSession } = createCookieSessionStorage<SessionData>();
+ *
+ * const session = getSession(astroCookies);
+ * ```
+ *
+ * @example If no type is specified, any key can be handled with values of any type.
+ * ```ts
+ * export const { getSession } = createCookieSessionStorage();
+ * ```
+ *
+ * @example Specifying options. The following options are the default values.
+ * ```ts
+ * export const { getSession } = createCookieSessionStorage({
+ *   cookieName: "astro.session",
+ *   cookieSetOptions: {
+ *     httpOnly: true,
+ *     secure: import.meta.env.PROD,
+ *     path: undefined,
+ *     expires: undefined,
+ *     maxAge: undefined,
+ *   }
+ * });
+ * ```
+ *
+ * @example Specify type for flash messages.
+ * ```ts
+ * type FlashData = {
+ *   success: string;
+ *   notice: string;
+ *   alert: string;
+ *   error: string;
+ * };
+ *
+ * type SessionData = {
+ *   userId: string;
+ * };
+ *
+ * export const { getSession } = createCookieSessionStorage<SessionData, FlashData>();
+ * ```
+ */
 export function createCookieSessionStorage<
   T extends Record<string, any> = { [key: string]: any },
   F extends Record<string, any> = DefaultFlash,
 >(options?: Options) {
   return {
+    /**
+     * Prepare a session object from AstroCookies.
+     *
+     * @example Using in Astro pages.
+     * ```ts
+     * const session = getSession(Astro.cookies);
+     * ```
+     *
+     * @example Using in Astro API routes.
+     * ```ts
+     * export const POST: APIRoute = async ({ cookies }) => {
+     *   const session = getSession(cookies);
+     *   // ...
+     * };
+     * ```
+     */
     getSession: (cookies: Cookies) => Session.from<T, F>(new CookieStorage(cookies, options)),
   };
 }
