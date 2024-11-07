77088d0035965ab771235bf8d3f03f0103769af3
koyopro <koyopro2.0@gmail.com>
2024-11-07 22:34:38 +0900

    [accel-web] docs for searchFormFor

--- packages/accel-web/src/searchFormFor.ts
+++ packages/accel-web/src/searchFormFor.ts
@@ -1,6 +1,26 @@
 import { Search } from "accel-record/search";
 import { formFor } from "./form/index.js";
 
+/**
+ * Generates a search form for the given search object.
+ *
+ * @param s - The search object for which the form is generated.
+ * @returns The generated form with the namespace "q".
+ * @example
+ * ```astro
+ * ---
+ * import { searchFormFor } from "accel-web";
+ * import { Todo } from "src/models";
+ *
+ * const search = Todo.search({ title_cont: "foo" });
+ * const { Form, TextField, Submit } = searchFormFor(search);
+ * ---
+ * <Form method="get">
+ *   <TextField attr="title_cont" />
+ *   <Submit>Search</Submit>
+ * </Form>
+ * ```
+ */
 export const searchFormFor = (s: Search) => {
   return formFor(s, { namespace: "q" });
 };
