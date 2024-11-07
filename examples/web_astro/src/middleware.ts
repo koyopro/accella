7b502e53ebf40205d0d2f46cf864d2d62c08fcd0
koyopro <koyopro2.0@gmail.com>
2024-11-07 21:57:13 +0900

    [examples/web_astro] use Astro.params

--- examples/web_astro/src/middleware.ts
+++ examples/web_astro/src/middleware.ts
@@ -7,9 +7,10 @@ import { RequestParameters } from "accel-web";
 await initI18n();
 await initDatabase();
 
-export const onRequest = defineMiddleware(async ({ locals, cookies, request, redirect }, next) => {
+export const onRequest = defineMiddleware(async (context, next) => {
+  const { locals, cookies, request, redirect, params } = context;
   locals.session = getSession(cookies);
-  locals.params = await RequestParameters.from(request);
+  locals.params = await RequestParameters.from(request, params);
 
   const path = new URL(request.url).pathname;
   if (!locals.session.account && !path.startsWith("/sign")) {
