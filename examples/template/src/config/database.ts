bb9bab397f1feca52d271f62964efec0f638570d
koyopro <koyopro2.0@gmail.com>
2024-11-11 02:26:02 +0900

    [examples/template] Accel.root

--- examples/template/src/config/database.ts
+++ examples/template/src/config/database.ts
@@ -1,8 +1,13 @@
 import { initAccelRecord } from "accel-record";
+import { Accel } from "./application";
 
 export const setupDatabase = async () => {
+  const sqlitePath = Accel.root.join(
+    "./db/schema/",
+    import.meta.env.DATABASE_URL.replace("file:", "")
+  );
   await initAccelRecord({
     type: "sqlite",
-    datasourceUrl: process.env.DATABASE_URL,
+    datasourceUrl: sqlitePath.path,
   });
 };
