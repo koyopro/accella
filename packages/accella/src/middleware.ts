d7997210de4365e7e2642d809e74291558fb7614
koyopro <koyopro2.0@gmail.com>
2024-11-16 07:36:44 +0900

    [accella] prepare Session

--- packages/accella/src/middleware.ts
+++ packages/accella/src/middleware.ts
@@ -1,6 +1,8 @@
 import { RequestParameters } from "accel-web";
+import { getSession } from "./session";
 
 export const onRequest = async ({ locals, request, params }: any, next: any) => {
+  locals.session = getSession(request.cookies);
   locals.params = await RequestParameters.from(request, params);
   return next();
 };
