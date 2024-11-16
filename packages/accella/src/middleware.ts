5c8ba0412db64036d52f06b9b980eeffb9b3b418
koyopro <koyopro2.0@gmail.com>
2024-11-17 00:53:45 +0900

    [accella] error handling

--- packages/accella/src/middleware.ts
+++ packages/accella/src/middleware.ts
@@ -1,8 +1,22 @@
+import { RecordNotFound } from "accel-record/errors";
 import { RequestParameters } from "accel-web";
+import { APIContext } from "astro";
+import { ZodError } from "zod";
 import { getSession } from "./session";
 
-export const onRequest = async ({ locals, request, params, cookies }: any, next: any) => {
+export const onRequest = async (context: APIContext, next: any) => {
+  const { cookies, request, params, locals } = context;
   locals.session = getSession(cookies);
   locals.params = await RequestParameters.from(request, params);
-  return next();
+  try {
+    return await next();
+  } catch (error) {
+    if (error instanceof RecordNotFound) {
+      return new Response(null, { status: 404 });
+    }
+    if (error instanceof ZodError) {
+      return new Response(null, { status: 400 });
+    }
+    throw error;
+  }
 };
