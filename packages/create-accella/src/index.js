aa70b5cc5bb8871fbf7e763e1bcaf1c081d077a8
koyopro <koyopro2.0@gmail.com>
2024-11-19 05:57:37 +0900

    [create-accella] generate secret key base on create

--- packages/create-accella/src/index.js
+++ packages/create-accella/src/index.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
 import { execSync } from "child_process";
+import { randomBytes } from "crypto";
+import { appendFileSync } from "fs";
 import { downloadTemplate } from "giget";
+import path from "path";
 
 (async () => {
   const project = process.argv[2] || "hello-accella";
@@ -9,6 +12,8 @@ import { downloadTemplate } from "giget";
   await downloadTemplate("github:koyopro/accella/examples/basics", {
     dir: project,
   });
+  const secretKeyBase = randomBytes(64).toString("hex");
+  appendFileSync(path.join(project, ".env"), `SECRET_KEY_BASE=${secretKeyBase}\n`);
 
   process.chdir(project);
   execSync("npm install", { stdio: "inherit" });
