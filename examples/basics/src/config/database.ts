fbbb04be14f7691794ffd375c1dec8df0ed03cdc
koyopro <koyopro2.0@gmail.com>
2024-11-19 22:33:01 +0900

    [accella] setup database

--- examples/basics/src/config/database.ts
+++ examples/basics/src/config/database.ts
@@ -1,6 +1,6 @@
 import { initAccelRecord } from "accel-record";
 import { getDatabaseConfig } from "../models";
 
-export const setupDatabase = async () => {
+export default async () => {
   await initAccelRecord(getDatabaseConfig());
 };
