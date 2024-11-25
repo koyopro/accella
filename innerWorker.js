61e57fa3f01d55b21e5a48d8208b9aa2c27dcd03
koyopro <koyopro2.0@gmail.com>
2024-11-26 01:25:49 +0900

    read file

--- innerWorker.js
+++ innerWorker.js
@@ -5,8 +5,20 @@ import { parentPort, workerData } from "worker_threads";
 const sharedArray = new Int32Array(workerData.sharedBuffer);
 
 // 内部スレッドの処理
-setTimeout(() => {
-  parentPort.postMessage("Hello from innerWorker");
-  Atomics.store(sharedArray, 0, 1);
-  Atomics.notify(sharedArray, 0, 1);
-}, 1000);
+
+parentPort.on("message", (buffer) => {
+  console.log(buffer);
+  const data = new Uint8Array(buffer);
+  const fileContent = Buffer.from(data).toString();
+  console.log("InnerWorker: Received file content:", fileContent);
+
+  // // 内部スレッドの処理
+  // setTimeout(() => {
+  //   parentPort.postMessage('Hello from innerWorker');
+  // }, 1000);
+  setTimeout(() => {
+    parentPort.postMessage("Hello from innerWorker");
+    Atomics.store(sharedArray, 0, 1);
+    Atomics.notify(sharedArray, 0, 1);
+  }, 1000);
+});
--- subWorker.js
+++ subWorker.js
@@ -1,5 +1,6 @@
 // subWorker.js
 // const { Worker, parentPort } = require('worker_threads');
+import fs from "fs";
 import { Worker, parentPort } from "worker_threads";
 
 const sharedBuffer = new SharedArrayBuffer(4);
@@ -24,7 +25,23 @@ subWorker.on("exit", (code) => {
   Atomics.notify(sharedArray, 0, 1);
 });
 
-console.log("SubWorker: Waiting for InnerWorker to finish processing...");
-// サブスレッドで内部スレッドの処理を同期的に待つ
-Atomics.wait(sharedArray, 0, 0);
-console.log("SubWorker: InnerWorker has finished processing.");
+// 内部スレッドを起動した後にファイルを渡す
+// const filePath = path.resolve("CONTRIBUTING.md");
+fs.readFile("./CONTRIBUTING.md", (err, data) => {
+  if (err) {
+    console.error("Error reading file:", err);
+    return;
+  }
+  const buffer = data.buffer.slice(data.byteOffset, data.byteOffset + data.byteLength);
+  console.log(buffer);
+  subWorker.postMessage(buffer, [buffer]);
+  console.log("File sent to innerWorker.");
+  console.log(buffer);
+
+  console.log("SubWorker: Waiting for InnerWorker to finish processing...");
+  // サブスレッドで内部スレッドの処理を同期的に待つ
+  Atomics.wait(sharedArray, 0, 0);
+  console.log("SubWorker: InnerWorker has finished processing.");
+
+  subWorker.terminate();
+});
