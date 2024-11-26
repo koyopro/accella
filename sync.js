349a0f833025427748a10e3f43ba3230f27bb9f7
koyopro <koyopro2.0@gmail.com>
2024-11-27 04:28:59 +0900

    useAction

--- sync.js
+++ sync.js
@@ -3,10 +3,10 @@ import actions from "./worker.js";
 const client = actions.launch();
 
 console.log(client.ping());
-// console.log(client.incr(3));
-// console.log(client.magic(0));
-// console.log(client.magic(1));
-// console.log(client.magic(2));
+console.log(client.incr(3));
+console.log(client.magic(0));
+console.log(client.magic(1));
+console.log(client.magic(2));
 
 setTimeout(() => {
   actions.stop();
--- worker.js
+++ worker.js
@@ -6,17 +6,17 @@ import {
   receiveMessageOnPort,
 } from "worker_threads";
 
-// const s = 0;
-
 // eslint-disable-next-line
-export const defineThreadSyncActions = (filename) => {
+export const defineThreadSyncActions = (filename, actions) => {
   console.log(filename);
+  useAction(actions);
   let worker = null;
   return {
     stop: () => {
       worker?.terminate();
       worker = null;
     },
+
     launch: () => {
       const sharedBuffer = new SharedArrayBuffer(4);
       const sharedArray = new Int32Array(sharedBuffer);
@@ -25,29 +25,38 @@ export const defineThreadSyncActions = (filename) => {
         workerData: { sharedBuffer, workerPort },
         transferList: [workerPort],
       });
-      return {
-        ping: () => {
-          worker.postMessage({ method: "ping" });
+      const ret = {};
+      for (const key of Object.keys(actions)) {
+        ret[key] = (...args) => {
+          worker.postMessage({ method: key, args });
           Atomics.wait(sharedArray, 0, 0);
-          const ret = receiveMessageOnPort(mainPort)?.message;
-          return ret;
-        },
-        // incr: (a) => a + 1,
-        // magic: (t) => ++s + t,
-      };
+          const ret = receiveMessageOnPort(mainPort);
+          Atomics.store(sharedArray, 0, 0);
+          console.log("ret", ret);
+          return ret?.message;
+        };
+      }
+      return ret;
     },
   };
 };
 
-// 内部スレッドの処理
-parentPort?.on("message", async (mgs) => {
-  const sharedArray = new Int32Array(workerData.sharedBuffer);
-  console.log("InnerWorker: Received message from parent.", mgs);
-  await new Promise((r) => setTimeout(r, 300));
-  workerData.workerPort.postMessage("pong!!!");
-  const value = 3;
-  Atomics.store(sharedArray, 0, value);
-  Atomics.notify(sharedArray, 0, 1);
-});
+const useAction = (actions) => {
+  // スレッドの処理
+  parentPort?.on("message", async (mgs) => {
+    const sharedArray = new Int32Array(workerData.sharedBuffer);
+    console.log("InnerWorker: Received message from parent.", mgs);
+    const ret = await actions[mgs.method]?.(...(mgs.args ?? []));
+    console.log("InnerWorker: Send message to parent.", ret);
+    workerData.workerPort.postMessage(ret);
+    Atomics.store(sharedArray, 0, 1);
+    Atomics.notify(sharedArray, 0, 1);
+  });
+};
 
-export default defineThreadSyncActions(import.meta.filename);
+let s = 0;
+export default defineThreadSyncActions(import.meta.filename, {
+  incr: async (a) => a + 1,
+  magic: (t) => ++s + t,
+  ping: () => "pong!?",
+});
