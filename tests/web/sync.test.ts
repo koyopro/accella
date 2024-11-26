5f39d4ac9cec502dd4b9c0644ecae9a649fd62a0
koyopro <koyopro2.0@gmail.com>
2024-11-27 05:00:47 +0900

    test for sync actions

---
+++ tests/web/sync.test.ts
@@ -0,0 +1,13 @@
+import actions from "../../worker";
+
+test("sync actinos", () => {
+  const client = actions.launch();
+
+  expect(client.ping()).toBe("pong!?");
+  expect(client.incr(3)).toBe(4);
+  expect(client.magic(0)).toBe(1);
+  expect(client.magic(1)).toBe(3);
+  expect(client.magic(2)).toBe(5);
+
+  actions.stop();
+});
