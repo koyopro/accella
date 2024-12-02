2bb67d80f3151ea9da32d42a86d40e55b914657e
koyopro <koyopro2.0@gmail.com>
2024-12-03 03:57:11 +0900

    Account#uploadAvatar()

--- examples/web_astro/src/models/account.ts
+++ examples/web_astro/src/models/account.ts
@@ -1,4 +1,14 @@
 import { hasSecurePassword, Mix } from "accel-record";
+import { actions } from "src/sync/actions.js";
 import { ApplicationRecord } from "./applicationRecord.js";
 
-export class AccountModel extends Mix(ApplicationRecord, hasSecurePassword()) {}
+export class AccountModel extends Mix(ApplicationRecord, hasSecurePassword()) {
+  avatar: File | undefined;
+
+  uploadAvatar() {
+    if (this.avatar) {
+      const result = actions.uploadImage(this.avatar, "kydev", "sync-test.png");
+      console.log(result);
+    }
+  }
+}
