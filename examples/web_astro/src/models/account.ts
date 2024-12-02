b89d79f46b15d92428a2c3bdf8b36540c2df3000
koyopro <koyopro2.0@gmail.com>
2024-12-03 05:24:03 +0900

    AvatarUploader

--- examples/web_astro/src/models/account.ts
+++ examples/web_astro/src/models/account.ts
@@ -2,26 +2,32 @@ import { hasSecurePassword, Mix } from "accel-record";
 import { actions } from "src/sync/actions.js";
 import { ApplicationRecord } from "./applicationRecord.js";
 
-export class AccountModel extends Mix(ApplicationRecord, hasSecurePassword()) {
-  avatar: File | undefined;
+export class AvatarUploader {
+  file: File | undefined;
 
-  constructor() {
-    super();
-    this.callbacks.before.save.push(this.uploadAvatar);
+  constructor(
+    private model: any,
+    public attr: string
+  ) {
+    model.callbacks.before.save.push(() => this.upload());
   }
 
-  get avatarUrl() {
-    if (!this.avatarPath) return undefined;
-
-    return `https://kydev.s3.ap-northeast-1.amazonaws.com/${this.avatarPath}`;
-  }
-
-  uploadAvatar() {
-    if (this.avatar) {
+  upload() {
+    if (this.file) {
       const fileName = `avatar-${Date.now()}.png`;
-      const result = actions.uploadImage(this.avatar, "kydev", fileName);
+      const result = actions.uploadImage(this.file, "kydev", fileName);
       console.log(result);
-      this.avatarPath = fileName;
+      this.model[this.attr] = fileName;
     }
   }
+
+  get url() {
+    if (!this.model[this.attr]) return undefined;
+
+    return `https://kydev.s3.ap-northeast-1.amazonaws.com/${this.model[this.attr]}`;
+  }
+}
+
+export class AccountModel extends Mix(ApplicationRecord, hasSecurePassword()) {
+  avatar = new AvatarUploader(this, "avatarPath");
 }
