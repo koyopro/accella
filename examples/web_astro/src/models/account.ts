0f95017c96544616a1f82252d02cb3fad774a772
koyopro <koyopro2.0@gmail.com>
2024-12-03 04:50:34 +0900

    uploadAvatar in callback

--- examples/web_astro/src/models/account.ts
+++ examples/web_astro/src/models/account.ts
@@ -5,6 +5,11 @@ import { ApplicationRecord } from "./applicationRecord.js";
 export class AccountModel extends Mix(ApplicationRecord, hasSecurePassword()) {
   avatar: File | undefined;
 
+  constructor() {
+    super();
+    this.callbacks.after.save.push(this.uploadAvatar);
+  }
+
   uploadAvatar() {
     if (this.avatar) {
       const result = actions.uploadImage(this.avatar, "kydev", "sync-test.png");
