50d30b5544977d77deaf49cab798a45fb11ddb8b
koyopro <koyopro2.0@gmail.com>
2024-12-04 03:13:31 +0900

    [accel-web] fix namespace bug in formFor()

---
+++ .changeset/cyan-apricots-cry.md
@@ -0,0 +1,5 @@
+---
+"accel-web": patch
+---
+
+Fixed the issue where the namespace passed to formFor() was not being reflected correctly.
--- packages/accel-web/tests/formFor.test.ts
+++ packages/accel-web/tests/formFor.test.ts
@@ -1,9 +1,17 @@
 import { experimental_AstroContainer as AstroContainer } from "astro/container";
 import MyForm from "./MyForm.astro";
+import MyFormWithNamespace from "./MyFormWithNamespace.astro";
 
 test("formFor() with FormModel", async () => {
   const container = await AstroContainer.create();
   const result = await container.renderToString(MyForm);
 
   expect(result).toContain('htmlFor="sampleForm.count"');
+});
+
+test("formFor() with namespace", async () => {
+  const container = await AstroContainer.create();
+  const result = await container.renderToString(MyFormWithNamespace);
+
+  expect(result).toContain('htmlFor="q.count"');
 });
