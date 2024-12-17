ef1da848abf4da28894425d279ad6bbf4dbe0cad
koyopro <koyopro2.0@gmail.com>
2024-12-18 03:24:01 +0900

    [accel-wave] BaseUploader

--- packages/accel-wave/src/index.ts
+++ packages/accel-wave/src/index.ts
@@ -1,8 +1,22 @@
 import { actions } from "./worker.js";
 
+export class BaseUploader {
+  storeDir = "uploads";
+  root = new URL("../public", import.meta.url).pathname;
+
+  constructor(options?: Partial<BaseUploader>) {
+    Object.assign(this, options);
+  }
+}
+
 export class FileStorage {
+  constructor(public uploader: BaseUploader) {}
+
   store(file: File) {
-    const filePath = new URL(`../public/uploads/${file.name}`, import.meta.url).pathname;
+    const filePath = new URL(
+      `${this.uploader.root}/${this.uploader.storeDir}/${file.name}`,
+      import.meta.url
+    ).pathname;
     actions.writeFile(filePath, file);
   }
 }
