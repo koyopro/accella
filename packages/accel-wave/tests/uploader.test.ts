2e6ed696e8c9215c00f3e169b9d602c06e22cdc4
koyopro <koyopro2.0@gmail.com>
2024-12-18 04:03:00 +0900

    [accel-wave] url()

--- packages/accel-wave/tests/uploader.test.ts
+++ packages/accel-wave/tests/uploader.test.ts
@@ -14,14 +14,22 @@ test("store()", () => {
   uploader.store(buildFile());
 
   expect(read("../public/uploads/example.txt")).toBe("Hello");
+
+  const regex = new RegExp("file:///.+/accel-wave/public/uploads/example.txt");
+  expect(regex.test(uploader.url()!.href)).toBeTruthy();
 });
 
 test("store() with customize", () => {
-  const uploader = new MyUploader({ root: "../tmp", storeDir: "custom" });
+  const uploader = new MyUploader({
+    root: "../tmp",
+    storeDir: "custom",
+    assetHost: "http://localhost",
+  });
 
   uploader.store(buildFile());
 
   expect(read("../tmp/custom/example.txt")).toBe("Hello");
+  expect(uploader.url()!.href).toBe("http://localhost/custom/myfile.txt");
 });
 
 const read = (filePath: string) => {
