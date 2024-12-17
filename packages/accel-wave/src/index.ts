39299f94c518ccf299b1230189da4abd9c052d1c
koyopro <koyopro2.0@gmail.com>
2024-12-18 03:36:21 +0900

    [accel-wave] custom filename

--- packages/accel-wave/src/index.ts
+++ packages/accel-wave/src/index.ts
@@ -7,14 +7,19 @@ export class BaseUploader {
   constructor(options?: Partial<BaseUploader>) {
     Object.assign(this, options);
   }
+
+  get filename(): string | undefined {
+    return undefined;
+  }
 }
 
 export class FileStorage {
   constructor(public uploader: BaseUploader) {}
 
   store(file: File) {
+    const filename = this.uploader.filename ?? file.name;
     const filePath = new URL(
-      `${this.uploader.root}/${this.uploader.storeDir}/${file.name}`,
+      `${this.uploader.root}/${this.uploader.storeDir}/${filename}`,
       import.meta.url
     ).pathname;
     actions.writeFile(filePath, file);
