360e2cd47fe43864ef70efadceae731cd71ffd1f
koyopro <koyopro2.0@gmail.com>
2024-12-18 03:24:01 +0900

    [accel-wave] BaseUploader

--- packages/accel-wave/tests/fileStorage.test.ts
+++ packages/accel-wave/tests/fileStorage.test.ts
@@ -1,12 +1,10 @@
 import fs from "fs";
 import path from "path";
-import { FileStorage } from "../src";
+import { BaseUploader, FileStorage } from "../src";
 
-test("store()", async () => {
-  const storage = new FileStorage();
-  const fileContent = new Uint8Array([72, 101, 108, 108, 111]); // Binary data for "Hello"
-  const blob = new Blob([fileContent], { type: "text/plain" });
-  const file = new File([blob], "example.txt", { type: "text/plain" });
+test("store()", () => {
+  const storage = new FileStorage(new BaseUploader());
+  const file = buildFile();
 
   storage.store(file);
 
@@ -14,3 +12,21 @@ test("store()", async () => {
   const content = fs.readFileSync(filePath, "utf-8");
   expect(content).toBe("Hello");
 });
+
+test("store() with storeDir", () => {
+  const storage = new FileStorage(new BaseUploader({ storeDir: "custom" }));
+  const file = buildFile();
+
+  storage.store(file);
+
+  const filePath = path.resolve(__dirname, "../public/custom/example.txt");
+  const content = fs.readFileSync(filePath, "utf-8");
+  expect(content).toBe("Hello");
+});
+
+const buildFile = () => {
+  const fileContent = new Uint8Array([72, 101, 108, 108, 111]); // Binary data for "Hello"
+  const blob = new Blob([fileContent], { type: "text/plain" });
+  const file = new File([blob], "example.txt", { type: "text/plain" });
+  return file;
+};
