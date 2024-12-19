3c432ec302ca50ce09cbbf7c8945898da34c72f8
koyopro <koyopro2.0@gmail.com>
2024-12-20 07:20:46 +0900

    [accel-wave] retrive S3 object

--- packages/accel-wave/src/storages/s3.ts
+++ packages/accel-wave/src/storages/s3.ts
@@ -1,7 +1,8 @@
 import type { Config } from "../config.js";
 import { actions } from "../worker.js";
+import { type Storage } from "./index.js";
 
-export class S3Storage {
+export class S3Storage implements Storage {
   constructor(public config: Config) {}
 
   store(file: File) {
@@ -16,5 +17,19 @@ export class S3Storage {
       },
       file
     );
+  }
+
+  retrive(identifier: string) {
+    const config = this.config.s3;
+    if (!config) throw new Error("S3 config is not set");
+
+    const byteArray = actions.loadS3(
+      { region: config.region },
+      {
+        Bucket: config.bucket,
+        Key: identifier,
+      }
+    );
+    return new File([new Blob([byteArray])], identifier);
   }
 }
