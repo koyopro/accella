5bf448840916dd91d7e7cfbbcde68fab3064cd9b
koyopro <koyopro2.0@gmail.com>
2024-12-20 05:42:38 +0900

    read file

--- tests/wave/mount.test.ts
+++ tests/wave/mount.test.ts
@@ -1,13 +1,19 @@
-import { $user } from "../factories/user";
 import fs from "fs";
+import { $Profile } from "../factories/profile";
+import { $user } from "../factories/user";
+import { Profile } from "../models";
 
 test("mount", () => {
-  const user = $user.create();
+  const user = $Profile.create({ user: $user.create() });
   const file = new File([], "example.png", { type: "image/png" });
   const target = `${process.cwd()}/public/uploads/example.png`;
   user.avatar.file = file;
   user.save();
   expect(fs.existsSync(target)).toBeTruthy();
   expect(user.avatarPath).toBe("example.png");
+
+  const p = Profile.find(user.id);
+  expect(p.avatar.file?.name).toBe("example.png");
+
   fs.unlinkSync(target);
 });
