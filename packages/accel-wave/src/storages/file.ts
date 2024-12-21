60a57677209628ea4bbeb85013b3bbd2ae73265e
koyopro <koyopro2.0@gmail.com>
2024-12-22 07:03:46 +0900

    [accel-wave] FileStorage delete

--- packages/accel-wave/src/storages/file.ts
+++ packages/accel-wave/src/storages/file.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import { basename } from "path";
 import { Config } from "../config.js";
 import { actions } from "../worker.js";
 import { type Storage } from "./index.js";
@@ -6,20 +7,19 @@ import { type Storage } from "./index.js";
 export class FileStorage implements Storage {
   constructor(public config: Config) {}
 
-  store(file: File) {
-    const filePath = new URL(
-      `${this.config.root}/${this.config.storeDir}/${this.config.filename}`,
-      import.meta.url
-    ).pathname;
+  store(file: File, identifier: string) {
+    const filePath = new URL(`${this.config.root}/${identifier}`, import.meta.url).pathname;
     actions.writeFile(filePath, file);
   }
 
   retrive(identifier: string) {
-    const filePath = new URL(
-      `${this.config.root}/${this.config.storeDir}/${identifier}`,
-      import.meta.url
-    ).pathname;
-    return new File([fs.readFileSync(filePath)], identifier);
+    const filePath = new URL(`${this.config.root}/${identifier}`, import.meta.url).pathname;
+    return new File([fs.readFileSync(filePath)], basename(filePath));
+  }
+
+  delete(identifier: string) {
+    const filePath = new URL(`${this.config.root}/${identifier}`, import.meta.url).pathname;
+    if (fs.existsSync(filePath)) fs.unlinkSync(filePath);
   }
 
   url(path: string) {
