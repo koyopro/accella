c4fd57c55e3fdd235795c1af7b942739cb61afa4
koyopro <koyopro2.0@gmail.com>
2024-12-22 01:41:14 +0900

    [accel-wave] add url to Storage intarface

--- packages/accel-wave/tests/s3Storage.test.ts
+++ packages/accel-wave/tests/s3Storage.test.ts
@@ -1,5 +1,6 @@
 import { config } from "dotenv";
 import { Config } from "src/config";
+import { BaseUploader } from "src/index";
 import { S3Storage } from "../src/storages/s3";
 import { buildFile } from "./buildFile";
 
@@ -22,6 +23,23 @@ test("store()", (context: any) => {
   expect(retrivedFile.name).toBe(file.name);
 
   expect(s3.url(file.name)).toMatch(
+    new RegExp(
+      `https://${bucket}.s3.${region}.amazonaws.com/example.txt\\?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=.+`
+    )
+  );
+});
+
+test("S3Storage with Uploader", () => {
+  const region = process.env.AWS_REGION;
+  const bucket = process.env.AWS_S3_BUCKET;
+  if (!region || !bucket) return;
+
+  const uploader = new BaseUploader({
+    storage: S3Storage,
+    s3: { region, bucket },
+    filename: "example.txt",
+  });
+  expect(uploader.url()).toMatch(
     new RegExp(
       `https://${bucket}.s3.${region}.amazonaws.com/example.txt\\?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=.+`
     )
