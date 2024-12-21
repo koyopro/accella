ff2ffc9288d6b7bde5aff6aa4ce96b844922a9ad
koyopro <koyopro2.0@gmail.com>
2024-12-22 02:57:36 +0900

    [accel-wave] improve Uploader

--- packages/accel-wave/src/index.ts
+++ packages/accel-wave/src/index.ts
@@ -1,5 +1,6 @@
 import type { Model } from "accel-record";
 import { Config } from "./config.js";
+import type { DeepPartial } from "./lib/deepMerge.js";
 import { type Storage } from "./storages/index.js";
 
 export { Config } from "./config.js";
@@ -8,22 +9,26 @@ export const mount = (model: Model, attr: string, uploader: BaseUploader) => {
   uploader.model = model;
   uploader.attr = attr;
   model.callbacks.before["save"].push(() => {
+    if (uploader.filename) {
+      (model as any)[attr] = uploader.filename;
+    }
+  });
+  model.callbacks.after["save"].push(() => {
     if (uploader.file) {
       uploader.store(uploader.file);
-      (model as any)[attr] = uploader.filename;
     }
   });
   return uploader;
 };
 
 export class BaseUploader extends Config {
-  protected _storage: Storage;
-  protected _file: File | undefined;
   model: Model | undefined;
   attr: string | undefined;
+  protected _storage: Storage;
+  protected _file: File | undefined;
   protected _filename: string | undefined;
 
-  constructor(options?: Partial<Config>) {
+  constructor(options?: DeepPartial<Config>) {
     super(options);
     this._storage = new this.storage(this);
   }
@@ -51,10 +56,11 @@ export class BaseUploader extends Config {
   url() {
     if (!this.filename) return undefined;
 
+    const path = `${this.storeDir}/${this.filename}`;
     if (this.assetHost) {
-      return new URL(`${this.storeDir}/${this.filename}`, this.assetHost);
+      return new URL(path, this.assetHost);
     } else {
-      return this._storage.url(`${this.storeDir}/${this.filename}`);
+      return this._storage.url(path);
     }
   }
 
