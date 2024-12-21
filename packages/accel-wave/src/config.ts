7e55d6910a89b47e8e7e4da99f6f3a8bd8b66fee
koyopro <koyopro2.0@gmail.com>
2024-12-22 02:48:22 +0900

    [accel-wave] Uploader extends Config

--- packages/accel-wave/src/config.ts
+++ packages/accel-wave/src/config.ts
@@ -3,15 +3,24 @@ import { FileStorage } from "./storages/file.js";
 import { type Storage } from "./storages/index.js";
 
 export class Config {
-  storage: new (config: Config) => Storage = FileStorage;
-  storeDir = "uploads";
-  root = `${process.cwd()}/public`;
-  assetHost: string | undefined;
-  filename: string | undefined;
-
   constructor(options?: DeepPartial<Config>) {
     deepMerge(this, globalConfig);
     deepMerge(this, options);
+
+    // Default values
+    this.storage ??= FileStorage;
+    this.storeDir ??= "uploads";
+    this.root ??= `${process.cwd()}/public`;
+  }
+}
+
+declare module "." {
+  interface Config {
+    storage: new (config: Config) => Storage;
+    storeDir: string;
+    root: string;
+    assetHost: string | undefined;
+    filename: string | undefined;
   }
 }
 
