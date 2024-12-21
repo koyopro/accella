9e0d9438f728e74ab50c0c9e5eca9ff85bd6eacb
koyopro <koyopro2.0@gmail.com>
2024-12-22 01:40:42 +0900

    [accel-wave] add storage to Config interface

--- packages/accel-wave/src/config.ts
+++ packages/accel-wave/src/config.ts
@@ -1,6 +1,9 @@
 import { deepMerge, type DeepPartial } from "./lib/deepMerge.js";
+import { FileStorage } from "./storages/file.js";
+import { type Storage } from "./storages/index.js";
 
 export class Config {
+  storage: new (config: Config) => Storage = FileStorage;
   storeDir = "uploads";
   root = `${process.cwd()}/public`;
   assetHost: string | undefined;
