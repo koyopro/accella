e4200caa308186edffe9b902864f3e5130ebf0f5
koyopro <koyopro2.0@gmail.com>
2024-12-23 00:40:42 +0900

    [accel-wave] build command

--- tests/models/_types.ts
+++ tests/models/_types.ts
@@ -34,7 +34,10 @@ import {
 declare module "accel-record" {
   function meta<T>(model: T): Meta<T>;
 
-  interface Relation<T, M> {}
+  interface Relation<T, M> {
+    john: (T extends User ? typeof UserModel['john'] : T extends Post ? typeof PostModel['john'] : never);
+    adults: (T extends User ? typeof UserModel['adults'] : never);
+  }
 }
 
 type Meta<T> = T extends typeof UserModel | UserModel ? UserMeta :
