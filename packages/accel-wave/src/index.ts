b3fe5105745d040be765358cfdaa49beb2f72bbc
koyopro <koyopro2.0@gmail.com>
2024-12-24 01:00:10 +0900

    [accel-wave] use Item

--- packages/accel-wave/src/index.ts
+++ packages/accel-wave/src/index.ts
@@ -17,13 +17,20 @@ export const mount = (model: Model, attr: string, uploader: BaseUploader) => {
   return uploader;
 };
 
+export class Item {
+  constructor(
+    public identifier: string | undefined,
+    public file: File
+  ) {}
+}
+
 export class BaseUploader extends Config {
   model: Model | undefined;
   attr: string | undefined;
   protected hasUpdate: boolean = false;
   protected _storage: Storage;
-  protected _file: File | undefined;
-  protected _filename: string | undefined;
+  protected item: Item | undefined;
+  protected removedItems: Item[] = [];
 
   constructor(options?: ConfigOptions) {
     super(options);
@@ -31,24 +38,25 @@ export class BaseUploader extends Config {
   }
 
   get filename() {
-    return this._filename;
+    return this.item?.identifier;
   }
   set filename(value: string | undefined) {
-    this._filename = value;
+    if (this.item) this.item.identifier = value;
   }
 
   get file() {
-    if (this._file) return this._file;
+    if (this.item?.file) return this.item.file;
     if (this.model && this.attr) {
       const identifier = (this.model as any)[this.attr];
       const path = `${this.storeDir}/${identifier}`;
-      return (this._file = this._storage.retrive(path));
+      this.item = new Item(identifier, this._storage.retrive(path));
+      return this.item.file;
     }
   }
 
   set file(file: File | undefined) {
-    this._file = file;
-    this._filename = file?.name;
+    if (this.item) this.removedItems.push(this.item);
+    this.item = file ? new Item(file.name, file) : undefined;
     this.hasUpdate = true;
   }
 
@@ -64,17 +72,17 @@ export class BaseUploader extends Config {
   }
 
   store(file?: File | undefined | null) {
-    const prevPath = `${this.storeDir}/${this.filename}`;
     if (file) this.file = file;
     if (file === null) this.file = undefined;
-    if (this.hasUpdate && this._file && this.filename) {
+    if (this.hasUpdate && this.item?.file && this.filename) {
       const path = `${this.storeDir}/${this.filename}`;
-      this._storage.store(this._file, path);
+      this._storage.store(this.item.file, path);
       this.hasUpdate = false;
     }
-    if (this.hasUpdate && !this._file) {
-      this._storage.delete(prevPath);
-      this.hasUpdate = false;
+    for (const item of this.removedItems) {
+      const path = `${this.storeDir}/${item.identifier}`;
+      this._storage.delete(path);
     }
+    this.removedItems = [];
   }
 }
