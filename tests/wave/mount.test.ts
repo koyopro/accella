7f5e197fbc3a5b1d844c35eee7a7cb5492f6d066
koyopro <koyopro2.0@gmail.com>
2024-12-25 00:37:31 +0900

    [accel-wave] update model in file setter

--- tests/wave/mount.test.ts
+++ tests/wave/mount.test.ts
@@ -5,9 +5,8 @@ import { Profile } from "../models";
 
 test("mount", () => {
   const profile = $Profile.create({ user: $user.create() });
-  const file = new File([], "example.png", { type: "image/png" });
   const target = `${process.cwd()}/public/uploads/example.png`;
-  profile.avatar.file = file;
+  profile.avatar.file = buildFile();
   profile.save();
   expect(fs.existsSync(target)).toBeTruthy();
   expect(profile.avatarPath).toBe("example.png");
@@ -18,3 +17,16 @@ test("mount", () => {
   p.destroy();
   expect(fs.existsSync(target)).toBeFalsy();
 });
+
+test("should be able to update only the column value", () => {
+  const profile = $Profile.create({ user: $user.create() });
+  profile.avatar.file = buildFile();
+  profile.save();
+  expect(profile.avatarPath).toBe("example.png");
+
+  profile.avatarPath = "example2.png";
+  profile.save();
+  expect(profile.avatarPath).toBe("example2.png");
+});
+
+const buildFile = () => new File([], "example.png", { type: "image/png" });
