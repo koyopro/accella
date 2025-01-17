c2c012c45d273fcb5953f6ee2ae4ad967946780e
koyopro <koyopro2.0@gmail.com>
2025-01-18 02:33:15 +0900

    [accel-web] Fixed the second argument of searchFormFor() to be optional instead of mandatory.

---
+++ .changeset/thirty-oranges-accept.md
@@ -0,0 +1,5 @@
+---
+"accel-web": patch
+---
+
+Fixed the second argument of searchFormFor() to be optional instead of mandatory.
--- tests/web/searchFormFor.test.ts
+++ tests/web/searchFormFor.test.ts
@@ -3,8 +3,16 @@ import { User } from "../models";
 
 test("searchFormFor", async () => {
   const search = User.search({});
-  const { Form, TextField, Submit } = searchFormFor(search, { namespace: "query" });
-  expect(Form).toBeDefined();
-  expect(TextField).toBeDefined();
-  expect(Submit).toBeDefined();
+  {
+    const { Form, TextField, Submit } = searchFormFor(search);
+    expect(Form).toBeDefined();
+    expect(TextField).toBeDefined();
+    expect(Submit).toBeDefined();
+  }
+  {
+    const { Form, TextField, Submit } = searchFormFor(search, { namespace: "query" });
+    expect(Form).toBeDefined();
+    expect(TextField).toBeDefined();
+    expect(Submit).toBeDefined();
+  }
 });
