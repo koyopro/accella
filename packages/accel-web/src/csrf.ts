253e9b762f40ea26230c4f495a6147e9f733e166
koyopro <koyopro2.0@gmail.com>
2025-01-20 06:10:55 +0900

    [accel-web] formAuthenticityToken()

--- packages/accel-web/src/csrf.ts
+++ packages/accel-web/src/csrf.ts
@@ -1,14 +1,27 @@
 import csrf from "csrf";
 
 const AUTHENTICITY_TOKEN_LENGTH = 32;
+const SESSION_KEY = "_csrf_token";
 
 export const formAuthenticityToken = (session: any) => {
-  const tokens = new csrf({
+  const tokens = newTokens();
+  const secret = tokens.secretSync();
+  const token = tokens.create(secret);
+  session.set(SESSION_KEY, token);
+  return token;
+};
+
+export const isValidAuthenticityToken = (session: any, token: string) => {
+  const secret = session.get(SESSION_KEY);
+  if (!secret) return false;
+
+  const tokens = newTokens();
+  return tokens.verify(secret, token);
+};
+
+const newTokens = () => {
+  return new csrf({
     saltLength: AUTHENTICITY_TOKEN_LENGTH,
     secretLength: AUTHENTICITY_TOKEN_LENGTH,
   });
-  const secret = tokens.secretSync();
-  const token = tokens.create(secret);
-  session.set("_csrf_token", token);
-  return token;
 };
--- packages/accel-web/tests/csrf.test.ts
+++ packages/accel-web/tests/csrf.test.ts
@@ -1,5 +1,5 @@
+import { formAuthenticityToken, isValidAuthenticityToken } from "src/csrf";
 import { createCookieSessionStorage } from "src/session";
-import { formAuthenticityToken } from "src/csrf";
 
 let data: Record<string, any> = {};
 const mockAstroCookies = {
@@ -20,9 +20,21 @@ type SessionData = {
 export const { getSession } = createCookieSessionStorage<SessionData>();
 const session = getSession(mockAstroCookies);
 
-test("formAuthenticityToken", async () => {
+test("formAuthenticityToken()", async () => {
   expect(session.get("_csrf_token")).toBeUndefined();
   const token = formAuthenticityToken(session);
   expect(token.length).toBeGreaterThanOrEqual(60);
   expect(session.get("_csrf_token")?.length).toBeGreaterThanOrEqual(60);
+});
+
+test("isValidAuthenticityToken()", async () => {
+  const validSecret = "RK7GvAduF_pYJRbNUCHFS_jFf2QgTkqBIkXujJ7Mn3U";
+  const validToken = "AmbcU4bhatotRJfZ3Ictudyq7tgD8ckf-sLYrOVfTTbDR3yMbsnR_KLLHDug";
+  const subject = (secret: any, token: string) => {
+    session.set("_csrf_token", secret);
+    return isValidAuthenticityToken(session, token);
+  };
+  expect(subject(validSecret, validToken)).toBe(true);
+  expect(subject(validSecret, validToken + "a")).toBe(false);
+  expect(subject(validSecret + "0", validToken)).toBe(false);
 });
