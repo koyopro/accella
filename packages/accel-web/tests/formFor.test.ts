ab051aeff6d1994f26a176f70525902909473090
koyopro <koyopro2.0@gmail.com>
2025-01-20 06:45:38 +0900

    [accel-web] set authenticity_token in Form

--- packages/accel-web/tests/formFor.test.ts
+++ packages/accel-web/tests/formFor.test.ts
@@ -1,12 +1,18 @@
 import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import { getMockSession } from "./mockSession";
 import MyForm from "./MyForm.astro";
 import MyFormWithNamespace from "./MyFormWithNamespace.astro";
 
+const session = getMockSession();
+
 test("formFor() with FormModel", async () => {
   const container = await AstroContainer.create();
-  const result = await container.renderToString(MyForm);
+  const result = await container.renderToString(MyForm, {
+    locals: { session },
+  });
 
   expect(result).toContain('htmlFor="sampleForm.count"');
+  expect(result).toContain('<input type="hidden" name="authenticity_token" value="');
 });
 
 test("formFor() with namespace", async () => {
