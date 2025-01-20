608f242177a31ea0ddf59e6e5be094e58dbe173f
koyopro <koyopro2.0@gmail.com>
2025-01-21 00:42:05 +0900

    [accel-web] Improve about formAuthenticityToken

--- packages/accel-web/src/csrf.ts
+++ packages/accel-web/src/csrf.ts
@@ -5,14 +5,14 @@ const SESSION_KEY = "_csrf_token";
 
 export const formAuthenticityToken = (session: any) => {
   const tokens = newTokens();
-  const secret = session.get(SESSION_KEY) ?? tokens.secretSync();
+  const secret: string = session.get(SESSION_KEY) ?? tokens.secretSync();
   const token = tokens.create(secret);
   session.set(SESSION_KEY, secret);
   return token;
 };
 
 export const isValidAuthenticityToken = (session: any, token: string) => {
-  const secret = session.get(SESSION_KEY);
+  const secret = session.get(SESSION_KEY) as string | undefined;
   if (!secret) return false;
 
   const tokens = newTokens();
