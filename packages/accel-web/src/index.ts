6ffd72375f58b3610945f868f11b6c51dfb5c581
koyopro <koyopro2.0@gmail.com>
2025-01-21 22:20:17 +0900

    [examples/web_astro] use CsrfMetaTags

--- packages/accel-web/src/index.ts
+++ packages/accel-web/src/index.ts
@@ -1,8 +1,10 @@
+import SortLink from "./SortLink.astro";
+import CsrfMetaTags from "./form/csrfMetaTags.astro";
+
 export { formFor } from "./form/index.js";
 export { paginate } from "./paginate/index.js";
 export { RequestParameters } from "./parameters.js";
 export { searchFormFor } from "./searchFormFor.js";
 export { createCookieSessionStorage, Session } from "./session.js";
 export { sortUrl } from "./sort.js";
-export { SortLink };
-import SortLink from "./SortLink.astro";
+export { CsrfMetaTags, SortLink };
