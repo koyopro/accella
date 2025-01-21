d88964b559f172167a9079ba0cfa164a07a14997
koyopro <koyopro2.0@gmail.com>
2025-01-22 00:07:56 +0900

    [accella] validateAuthenticityToken in middleware

--- examples/web_astro/src/middleware.ts
+++ examples/web_astro/src/middleware.ts
@@ -1,4 +1,3 @@
-import { formAuthenticityToken, validateAuthenticityToken } from "accel-web/csrf";
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware(async (context, next) => {
@@ -8,10 +7,6 @@ export const onRequest = defineMiddleware(async (context, next) => {
   if (!locals.session.account && !path.startsWith("/sign")) {
     return redirect("/signin");
   }
-
-  validateAuthenticityToken(locals.params, locals.session, request);
-
-  locals.authenticityToken = formAuthenticityToken(locals.session);
 
   return next();
 });
--- packages/accella/src/middleware.ts
+++ packages/accella/src/middleware.ts
@@ -4,6 +4,7 @@ import { APIContext } from "astro";
 import { ZodError } from "zod";
 import { runInitializers } from "./initialize.js";
 import { getSession } from "./session";
+import { formAuthenticityToken, validateAuthenticityToken } from "accel-web/csrf";
 
 await runInitializers();
 
@@ -11,6 +12,10 @@ export const onRequest = async (context: APIContext, next: any) => {
   const { cookies, request, params, locals } = context;
   locals.session = getSession(cookies);
   locals.params = await RequestParameters.from(request, params);
+  locals.authenticityToken = formAuthenticityToken(locals.session);
+
+  validateAuthenticityToken(locals.params, locals.session, request);
+
   try {
     return await next();
   } catch (error) {
