630882a5fcc21b16e7b4b15fcddc78d6b6ec5f7e
koyopro <koyopro2.0@gmail.com>
2025-02-01 01:52:54 +0900

    [examples/basics] Removed code for test from initializers/database.ts

--- examples/basics/src/config/initializers/database.ts
+++ examples/basics/src/config/initializers/database.ts
@@ -2,16 +2,5 @@ import { initAccelRecord } from "accel-record";
 import { getDatabaseConfig } from "../../models";
 
 export default async () => {
-  const config = getDatabaseConfig();
-
-  if (process.env.NODE_ENV === "test") {
-    // Vitest usually performs tests in a multi-threaded manner.
-    // To use different databases in each thread, separate the databases using VITEST_POOL_ID.
-    config.datasourceUrl = new URL(
-      `../../../db/test${process.env.VITEST_POOL_ID}.db`,
-      import.meta.url
-    ).pathname;
-  }
-
-  await initAccelRecord(config);
+  await initAccelRecord(getDatabaseConfig());
 };
