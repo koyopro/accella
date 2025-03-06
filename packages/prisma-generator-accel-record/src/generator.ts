df75b85b59798173803a503f6ad52481a5e54101
koyopro <koyopro2.0@gmail.com>
2025-03-07 02:01:43 +0900

    tmp

--- packages/prisma-generator-accel-record/src/generator.ts
+++ packages/prisma-generator-accel-record/src/generator.ts
@@ -1,12 +1,20 @@
 import { generatorHandler, GeneratorOptions } from "@prisma/generator-helper";
 import * as fs from "fs";
+import * as fsPromises from "fs/promises";
 import path from "path";
 import { GENERATOR_NAME } from "./constants";
 import { ensureApplicationRecord } from "./generators/applicationRecord";
 import { generateFactory } from "./generators/factory";
-import { generateIndex, toCamelCase } from "./generators/index";
+import {
+  generateIndex,
+  toCamelCase,
+  generateSchemaFileContent,
+  ACCEL_RECORD_DIR,
+  SCHEMA_CONFIG_FILE,
+} from "./generators/index";
 import { getModel as generateModel } from "./generators/model";
 import { writeFileSafely } from "./utils/writeFileSafely";
+import { mkdirp } from "mkdirp";
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const { version } = require("../package.json");
@@ -23,6 +31,10 @@ generatorHandler({
   },
   onGenerate: async (options: GeneratorOptions) => {
     const outputDir = options.generator.output!.value!;
+
+    // スキーマ設定ファイルの出力
+    await writeSchemaFile(options);
+
     await writeFileSafely(path.join(outputDir, `_types.ts`), await generateIndex(options));
     const indexFile = path.join(outputDir, `index.ts`);
     if (!fs.existsSync(indexFile)) {
@@ -36,6 +48,24 @@ generatorHandler({
     await generateFactories(options, indexFile);
   },
 });
+
+// スキーマ設定ファイルを書き出す関数
+async function writeSchemaFile(options: GeneratorOptions): Promise<void> {
+  const outputPath = options.generator.output!.value!;
+  const basePath = path.resolve(outputPath, "../.."); // node_modulesの親ディレクトリを取得
+  const accelDir = path.join(basePath, ACCEL_RECORD_DIR);
+
+  // ディレクトリが存在しない場合は作成
+  await mkdirp(accelDir);
+
+  // スキーマ情報を生成
+  const content = generateSchemaFileContent(options);
+
+  // ファイルに書き出し
+  const filePath = path.join(accelDir, SCHEMA_CONFIG_FILE);
+  await fsPromises.writeFile(filePath, content, "utf8");
+  console.info(`${green("create")}: ${path.relative(currentDir, filePath)}`);
+}
 
 const green = (text: string) => `\x1b[32m${text}\x1b[39m`;
 
