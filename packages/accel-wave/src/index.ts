690a58a177208a8f5e5e5a1db00a64a35baf9c74
koyopro <koyopro2.0@gmail.com>
2025-03-19 02:06:46 +0900

    [accel-wave] add exports

--- packages/accel-wave/src/index.ts
+++ packages/accel-wave/src/index.ts
@@ -1,4 +1,7 @@
-export { Config } from "./config.js";
-export { worker } from "./worker/index.js";
+export { Config, configureAccelWave } from "./config.js";
 export { mount } from "./mount.js";
+export { FileStorage } from "./storages/file.js";
+export { type Storage } from "./storages/index.js";
+export { S3Storage } from "./storages/s3.js";
 export { BaseUploader, Item } from "./uploader.js";
+export { worker } from "./worker/index.js";
